6e27e030d5e9ed367cf3936cc550686d23d7d8b4
NatAnastas <anastasiadounat@gmail.com>
2025-03-22 11:15:14 +0200

    color

--- 2025/01/01_day.R
+++ 2025/01/01_day.R
@@ -33,9 +33,14 @@ label = paste0(df$pldb_id, "\n", df$number_of_users)
 # Label
 df[, label := paste0(pldb_id, "\n", scales::comma(number_of_users))]
 
+
+col = c('#60608b', '#9291be', '#b9b8e5', '#ffeacf','#fcc1ad', '#e7877d', '#c15451')
+
+
 # plot --------
 
-ggplot(df, aes(area = number_of_users, fill = pldb_id, label = label)) +
+ggplot(df, aes(area = number_of_users, fill = number_of_users, label = label)) +
+    
     geom_treemap(layout = "squarified", color = "#f8f2f9", start = "topleft", radius = unit(2, "pt")) +
     
     geom_treemap_text(
@@ -46,6 +51,15 @@ ggplot(df, aes(area = number_of_users, fill = pldb_id, label = label)) +
         grow = FALSE, 
         reflow = TRUE,
         min.size = .5) +
+    
+    # scale_fill_manual(values = col) +
+    scale_fill_stepsn(
+        colors = col, 
+        breaks = c(50000, 100000, 200000, 500000, 1000000, 5000000), 
+        transform = "log2",
+        guide = guide_colorsteps(barwidth = unit(16, "lines"), 
+                                 barheight = unit(.5, "lines"))
+    ) +
     
     theme(
         legend.position = "none"
