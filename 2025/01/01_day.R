ad48c925cbc3e7432c7dccf3d4149e8a011f7f4b
NatAnastas <anastasiadounat@gmail.com>
2025-03-22 11:03:58 +0200

    treemap

--- 2025/01/01_day.R
+++ 2025/01/01_day.R
@@ -8,22 +8,52 @@ gc()
 
 library(data.table)
 library(ggplot2)
-library(stringr)
-library(extrafont)
-library(colorspace)
-library(ggtext)
-library(paletteer)
-library(shadowtext)
+library(treemapify)
+
+# library(stringr)
+# library(extrafont)
+# library(colorspace)
+# library(ggtext)
+# library(paletteer)
+# library(shadowtext)
 
 # load data --------
 
-palmtrees <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-03-18/palmtrees.csv')
+languages <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2023/2023-03-21/languages.csv')
 
 
 # data cleaning ------
 
+df = languages[, .(pldb_id, number_of_users)]
+
+df = df[number_of_users > 10000, ]
+
+label = paste0(df$pldb_id, "\n", df$number_of_users)
+
+# Label
+df[, label := paste0(pldb_id, "\n", scales::comma(number_of_users))]
 
 # plot --------
+
+ggplot(df, aes(area = number_of_users, fill = pldb_id, label = label)) +
+    geom_treemap(layout = "squarified", color = "#f8f2f9", start = "topleft", radius = unit(2, "pt")) +
+    
+    geom_treemap_text(
+        colour = "white",
+        # size = 15,
+        layout = "squarified", 
+        start = "topleft", 
+        grow = FALSE, 
+        reflow = TRUE,
+        min.size = .5) +
+    
+    theme(
+        legend.position = "none"
+    )
+
+
+
+
 
 
 # save ---------
