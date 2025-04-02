bb9fdb39cfc32c4056e265e24778500246c37ca2
NatAnastas <anastasiadounat@gmail.com>
2025-04-02 11:28:12 +0300

    axis

--- 2025/05/05_day.R
+++ 2025/05/05_day.R
@@ -25,11 +25,13 @@ taylor_album_songs <- fread('https://raw.githubusercontent.com/rfordatascience/t
 
 d = taylor_album_songs[, .(album_name, track_number, track_name, danceability, energy)]
 
+d = d[!is.na(danceability), ]
+
 # Plot -----------
 
-ggplot(d, aes(x = track_number, y = album_name, group = album_name)) +
+gr = ggplot(d, aes(x = track_number, y = album_name, group = album_name)) +
     
-    geom_line(color = "grey65", size = 0.6, alpha = 0.7) +
+    # geom_line(color = "grey75", size = 0.5, alpha = 0.7) +
     
     geom_point(
         aes(fill = energy, size = danceability),
@@ -38,7 +40,10 @@ ggplot(d, aes(x = track_number, y = album_name, group = album_name)) +
         alpha = 0.9
     ) +
     
-    scale_size(range = c(1, 6), guide = guide_legend(title = "Danceability")) +  # Adjust the point size range
+    scale_size(
+        range = c(1.5, 5), 
+        guide = guide_legend(title = "Danceability")
+    ) +  # Adjust the point size range
     
     
     scale_fill_stepsn(
@@ -46,7 +51,7 @@ ggplot(d, aes(x = track_number, y = album_name, group = album_name)) +
         colors =  c('#2c5769', '#6F99AD', 'grey96', '#ffb5ac', '#b1532a'),
         # breaks = c(1000, 1200, 1400),
         guide = guide_colorsteps(
-            title = "Bubble Color",
+            title = "Energy",
             barheight = unit(7, "lines"),
             barwidth = unit(0.5, "lines")
             
@@ -58,7 +63,8 @@ ggplot(d, aes(x = track_number, y = album_name, group = album_name)) +
     labs(
         title = " Danceability & Energy Levels in Taylor Swift’s Albums.",
         subtitle = "Tracks are ranked by their order in each album, with <b>bubble size</b> representing <b>danceability</b> and <b>color</b> indicating <b>energy levels</b>.",
-        caption = "Source: <b>  {taylor} R Package</b> | Graphic: <b>Natasa Anastasiadou</b>"
+        caption = "Source: <b>  {taylor} R Package</b> | Graphic: <b>Natasa Anastasiadou</b>",
+        x = "Track number"
     ) +
     
     theme(
@@ -70,16 +76,17 @@ ggplot(d, aes(x = track_number, y = album_name, group = album_name)) +
         legend.text = element_text(size = 7, family = "Candara", color = "grey30"),
 
         axis.title.y = element_blank(),
-        # axis.title.x = element_blank(),
-        # axis.text.x = element_blank(), 
-        # axis.text.y = element_blank(), 
+        
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size = 10, family = "Candara"),
+        axis.text.y = element_text(size = 10, family = "Candara"),
         
         panel.grid.major = element_line(linewidth = .35, color = "grey85"),
         panel.grid.minor = element_blank(),
         
-        plot.title = element_markdown(size = 15, face = "bold", hjust = 0.5, family = "Candara", margin = margin(t = 15)),
-        plot.subtitle = element_markdown(size = 11, hjust = 0.5, family = "Candara", color = "grey30", margin = margin(t = 5, b = 10)),
-        plot.caption = element_markdown(margin = margin(t = 12), size = 8, family = "Candara", hjust = 1.3),
+        plot.title = element_markdown(size = 14, face = "bold", hjust = 0.5, family = "Candara", margin = margin(t = 15, b = 5)),
+        plot.subtitle = element_markdown(size = 10, hjust = 0.65, family = "Candara", color = "grey30", margin = margin(t = 5, b = 15)),
+        plot.caption = element_markdown(margin = margin(t = 12), size = 8, family = "Candara", hjust = 1.35),
         
         plot.margin = margin(20, 20, 20, 20),
         
