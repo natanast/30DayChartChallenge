33efda46eee88cc52bd76a4aa7b0c72ecd00c236
NatAnastas <anastasiadounat@gmail.com>
2025-04-08 19:20:58 +0300

    09

--- 2025/09/09_day.R
+++ 2025/09/09_day.R
@@ -15,49 +15,71 @@ library(extrafont)
 
 # load data --------
 
-ratings  <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2022/2022-01-25/ratings.csv')
-# details <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2022/2022-01-25/details.csv')
+ratings <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2022/2022-01-25/ratings.csv')
 
 
+# data cleaning -----------
 
-# Plot 1 -----------
+index <- str_detect(ratings$name, "D&D|Dungeons & Dragons")
+
+df <- ratings[index, ]
 
 
-# Filter for D&D-themed games (Assuming you have a 'category' column or similar to filter D&D games)
-df_dnd <- ratings %>% 
-    filter(grepl("D&D|Dungeons & Dragons", name, ignore.case = TRUE))  # adjust according to the actual column name
+df$name <- df$name |> 
+    str_remove_all("D&D|Dungeons & Dragons|:") |>  
+    str_squish()
+
+
 
 # Calculate the rating deviation
-avg_rating <- mean(df_dnd$average, na.rm = TRUE)
-df_dnd <- df_dnd %>% 
-    mutate(rating_dev = average - avg_rating)
+avg_rating <- mean(df$average, na.rm = TRUE)
 
-# Reshape the data back to long format
-df_long <- df_dnd %>%
-    select(name, rating_dev) %>%
-    pivot_longer(cols = -name, names_to = "game_name", values_to = "rating_dev")
-
-# Check the reshaped data
-head(df_long)
+df[ , rating_dev := average - avg_rating]
 
 
+df_long <- melt(
+    df,
+    id.vars = "name",
+    measure.vars = "rating_dev",
+    variable.name = "game_name",
+    value.name = "rating_dev"
+)
 
-# lollipop plot
-ggplot(df_dnd, aes(x = reorder(name, rating_dev), y = rating_dev, color = rating_dev)) +
-    geom_segment(aes(xend = name, yend = 0), size = 1) +  # Draw line from zero to rating
-    geom_point(size = 4) +  # Dot at the end of the line
-    scale_color_gradient2(midpoint = 0, low = "red", high = "green", mid = "white") +
+
+# plot ---------
+
+library(paletteer)
+
+color_palette <- paletteer::paletteer_d("palettetown::pidgey")[c(9, 8, 2, 5, 4, 6, 12, 6, 3, 11)]
+
+gr =df |>
+    ggplot(aes(x = reorder(name, rating_dev), y = rating_dev, color = rating_dev)) +
+    
+    geom_segment(aes(xend = name, yend = 0), size = 1) +
+    
+    geom_point(size = 4) +
+    
+    scale_color_gradientn(colors = color_palette) +
+    
     labs(
         title = "Diverging Lollipop Chart of D&D Board Game Rating Deviations",
         x = "Board Game",
         y = "Rating Deviation"
     ) +
+    
     theme_minimal() +
+    
     theme(
-        axis.text.x = element_text(angle = 45, hjust = 1, family="Candara", size = 10),
-        plot.title = element_text(size = 14, color = 'black', hjust = 0.5, family="Candara"),
-        plot.background = element_rect(fill='white'),
-        panel.background = element_rect(fill='white')
+        axis.text.x = element_text(angle = 90, hjust = 1, size = 8, family = "Candara", color = "#e4e4e3"),
+        
+        plot.title = element_markdown(size = 16, face = "bold", color = "#e4e4e3", hjust = 0.5, family = "Candara", margin = margin(t = 15, b = 5)),
+        plot.subtitle = element_markdown(size = 11, hjust = 0.5, family = "Candara", color = "#e4e4e3", margin = margin(t = 5, b = 25)),
+        plot.caption = element_markdown(margin = margin(t = 5), size = 8, family = "Candara", hjust = 1.25),
+
+        plot.margin = margin(20, 20, 20, 20),
+        
+        plot.background = element_rect(fill = "grey35", color = NA)
+        
     )
 
 
