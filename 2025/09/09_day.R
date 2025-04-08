6e8047c57d269b79943592270995e93d7efe434b
NatAnastas <anastasiadounat@gmail.com>
2025-04-08 22:43:25 +0300

    p

--- 2025/09/09_day.R
+++ 2025/09/09_day.R
@@ -91,7 +91,7 @@ gr = df |>
     labs(
         title = "Diverging Lollipop Chart of D&D Board Game Rating Deviations",
         subtitle = "",
-        caption = "",
+        caption = "Source: <b> Board Games Data</b> | Graphic: <b>Natasa Anastasiadou</b>",
         x = "Board Game",
         y = "Rating Deviation"
     ) +
@@ -122,64 +122,6 @@ gr = df |>
 
 gr
 
-
-
-# plot -----------
-
-# ggplot(avg_risk_dt, aes(x = factor(Group), y = Proportion, fill = Group)) +
-#     
-#     geom_bar(
-#         position = "stack", 
-#         stat = "identity", 
-#         width = 1, 
-#         alpha = 0.9, 
-#         color = "black",
-#         linewidth = 0.25,
-#         fill = "#ACD4EC"
-#     ) +
-#     
-#     coord_polar(start = 0) + 
-#     
-#     geom_text(data = label_data, aes(x = id, y = Proportion, label = Group, angle = angle),
-#               color = "black", fontface = "bold", alpha = 0.6, size = 2.3, inherit.aes = FALSE,
-#               family = "Candara") +
-# 
-# 
-#     theme_minimal() +
-#     
-#     # labs(title = "Circular Barplot of Proportions of Conditions",
-#     #      subtitle = "Proportion of individuals with each condition (binary 0/1)",
-#     #      x = "Group",
-#     #      y = "Proportion of 1's") +
-#     
-# 
-#     theme(
-# 
-#         legend.position = "none",
-#         legend.title.position = "left",
-# 
-#         legend.title = element_blank(),
-# 
-#         axis.title.x = element_blank(),
-# 
-#         axis.title.y = element_blank(),
-#         axis.text.x = element_blank(),
-#         axis.text.y = element_blank(),
-# 
-#         panel.grid.major = element_blank(),
-#         panel.grid.minor = element_line(linewidth = .35, color = "grey80"),
-# 
-#         plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, family = "Candara", margin = margin(t = 15, b = 5)),
-#         plot.subtitle = element_markdown(size = 11, hjust = 0.5, family = "Candara", color = "grey30", margin = margin(t = 5, b = 25)),
-#         plot.caption = element_markdown(margin = margin(t = 5), size = 8, family = "Candara", hjust = 1.25),
-# 
-#         plot.margin = margin(6, 6, 6, 6),
-# 
-#         plot.background = element_rect(fill = "#e4e4e3", color = NA)
-#     )
-# 
-# gr
-# 
 
 # save ---------
 
