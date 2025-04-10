27bfe3f7402a6c42e679cf2673d460995ea43187
NatAnastas <anastasiadounat@gmail.com>
2025-04-10 21:03:44 +0300

    plot

--- 2025/11/11_day.R
+++ 2025/11/11_day.R
@@ -30,15 +30,15 @@ df <- global_temps[1:145, 1:13]
 
 df <- df[Year >= 1990, ]
 
-# Convert month columns to numeric
 df[, 2:13] <- lapply(df[, 2:13], function(x) as.numeric(as.character(x)))
 
 
-# Melt the data into long format for ggplot
 long_data <- melt(df, id.vars = "Year", variable.name = "Month", value.name = "Temperature Anomaly")
 
+long_data$Month <- factor(long_data$Month, levels = rev(unique(long_data$Month)))
 
 col = c("#00429d", "#73a2c6", "#ffffe0", "#ff9a92", "#b24745")
+
 
 # plot --------
 
@@ -49,19 +49,19 @@ gr <- ggplot(long_data, aes(x = Year, y = Month, fill = `Temperature Anomaly`)) 
     scale_fill_gradientn(
         colors = col,
         guide = guide_colorbar(
-            barwidth = unit(0.3, "cm"),  # Make it thinner (adjust as needed)
-            barheight = unit(5, "cm")    # Adjust height if you want too
+            barwidth = unit(0.3, "cm"),
+            barheight = unit(5, "cm")
         ),
-        name = "Temperature"
+        name = "Temperature",
+        labels = function(x) paste0(x, "°C")
     ) +
-    
 
     
     theme_minimal() +
     
     labs(
-        title = "Global surface temperatures changes",
-        subtitle = "Monthly temperature changes from 1990 to 2024",
+        title = "Global Surface Temperature Anomalies (°C)",
+        subtitle = "Monthly deviations from the 1951–1980 average, from 1990 to 2024",
         caption = "Source: <b> NOAA Global Temperature Data</b> | Graphic: <b>Natasa Anastasiadou</b>",
         y = "",
         x = ""
@@ -73,12 +73,12 @@ gr <- ggplot(long_data, aes(x = Year, y = Month, fill = `Temperature Anomaly`)) 
         legend.title = element_text(size = 10, face = "bold", family = "Candara", angle = 90, color = "grey30", hjust = .5),
         legend.text = element_text(size = 8, family = "Candara", color = "grey30"),
         
-        axis.text.x = element_text(size = 12, family = "Candara", angle = 90, hjust = 1, vjust = .25, margin = margin(t = 2)),
-        axis.text.y = element_text(size = 12, family = "Candara"),
+        axis.text.x = element_text(size = 12, family = "Candara", angle = 90, hjust = 1, vjust = .35, margin = margin(b = -20)),
+        axis.text.y = element_text(size = 12, family = "Candara", margin = margin(r = -15)),
         
         plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, family = "Candara", margin = margin(b = 5, t = 5)),
         plot.subtitle = element_markdown(size = 12, hjust = 0.5, family = "Candara", color = "grey30", margin = margin(b = 15, t = 5)),
-        plot.caption = element_markdown(margin = margin(t = 35), size = 8, family = "Candara", hjust = 1),
+        plot.caption = element_markdown(margin = margin(t = 35), size = 8, family = "Candara", hjust = 1.15),
         
         plot.margin = margin(20, 20, 20, 20),
         plot.background = element_rect(fill = "grey93", color = NA)
