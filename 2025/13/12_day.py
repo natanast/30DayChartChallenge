c3e89a9aa52543c4940c99909f7168b39e15435b
NatAnastas <anastasiadounat@gmail.com>
2025-04-12 13:17:28 +0300

    p

--- 2025/13/12_day.py
+++ 2025/13/12_day.py
@@ -21,7 +21,7 @@ df_clean = tornados.dropna(subset=['inj', 'fat'])
 
 # Calculate median values to draw the lines
 line_1 = 450
-line_2 = 40
+line_2 = 60
 
 # Create a new column to categorize points into quadrants based on their position
 df_clean['quadrant'] = pd.cut(
@@ -47,10 +47,6 @@ g = (
     
     geom_point(alpha = 0.85, size = 3) +
 
-    # + facet_grid('stat~type_1')  # One row per stat, one column per type
-
-    # + scale_fill_manual(values = col)
-
     # Add lines to separate the plot into 4 quadrants
     geom_vline(xintercept = line_1, color = "#7B726F", linetype = "dashed", size = 0.5) + # Vertical line at median injuries
     geom_hline(yintercept = line_2, color = "#7B726F", linetype = "dashed", size = 0.5) + # Horizontal line at median fatalities
@@ -59,9 +55,9 @@ g = (
     theme_minimal(base_family = "Candara") +
     
     labs(
-        title="Tornado Injuries vs Fatalities",
-        # subtitle="Histograms of key stats (Attack, Defense, Speed) for Electric, Fire, Grass, and Water types",
-        # caption="Source: {pokemon} R package | Graphic: Natasa Anastasiadou",
+        title = "Tornado Injuries vs Fatalities: A State-by-State Breakdown",
+        subtitle = "Exploring the relationship between the number of injuries and fatalities caused by tornadoes across U.S. states.",
+        caption = "Source: Tornado dataset | Graphic: Natasa Anastasiadou",
         x = 'No of Injuries',
         y = 'No of Fatalities'
     ) +
@@ -71,17 +67,15 @@ g = (
         
         # axis_text = element_text(family = 'Candara', size = 8),
         # axis_title = element_text(family = 'Candara', size = 9),
-
-        # plot_title = element_text(size = 12, weight='bold', family='Candara', ha='center'),
-        # plot_subtitle = element_text(size = 10, family='Candara', ha='center'),
-        # plot_caption = element_text(size = 7, family='Candara', ha='right'),
+ 
+        plot_title = element_text(size = 12, weight='bold', ha='center'),
+        plot_subtitle = element_text(size = 10, ha='center'),
+        plot_caption = element_text(size = 7, ha='right'),
         
         # strip_text = element_text(size = 8, family='Candara'),
         
         panel_grid_major = element_line(color = '#c9c9c9', alpha = 0.75, size = 0.65, linetype = "dashed"),
         panel_grid_minor = element_blank(),
-
-        # panel_border = element_rect(color = '#e5e5e5', alpha = 0.7, size = 0.5, fill = None),
         
         plot_background = element_rect(fill = '#e9e9e9', color = '#e9e9e9'),
         panel_background = element_rect(fill = '#e9e9e9', color = '#e9e9e9'),
@@ -89,7 +83,14 @@ g = (
         # axis_ticks= element_blank(),
         
         figure_size = (10, 6)
-    )
+    ) 
+
+    #  Add text labels for each quadrant with static text
+    # geom_text(x = line_1 - 350, y = line_2 - 10, label = "Low Injuries, Low Fatalities", color = "#6F99AD", size = 8, fontweight = 'bold') +
+    # geom_text(x = line_1 + 1000, y = line_2 - 5 , label="High Injuries, Low Fatalities", color = "#D78D50", size = 8, fontweight = 'bold') +
+    # geom_text(x = line_1 - 350, y = line_2 + 5, label="Low Injuries, High Fatalities", color = "#BC3C29", size = 8, fontweight = 'bold') +
+    # geom_text(x = line_1 + 1000, y = line_2 + 80, label="High Injuries, High Fatalities", color = "#6f6e9a", size = 8, fontweight='bold')
+
 )
 
 g
