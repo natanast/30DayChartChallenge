190ba9591319087889e3179fb5c2bc298515ec3f
NatAnastas <anastasiadounat@gmail.com>
2025-04-12 12:37:03 +0300

    scatter

--- 2025/13/12_day.py
+++ 2025/13/12_day.py
@@ -13,68 +13,80 @@ tornados = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytu
 
 # clean data ------
 
-plt.rcParams["font.family"] = "Candara"
+# plt.rcParams["font.family"] = "Candara"
+
+# Remove rows with missing values in 'inj' or 'fat'
+df_clean = tornados.dropna(subset=['inj', 'fat'])
 
 
-df = pokemon_df[['pokemon', 'type_1'] + stats]
-df = df[df['type_1'].isin(selected_types)].dropna()
+# Calculate median values to draw the lines
+line_1 = 450
+line_2 = 40
 
+# Create a new column to categorize points into quadrants based on their position
+df_clean['quadrant'] = pd.cut(
+    df_clean['inj'],
+    bins=[-float('inf'), line_1, float('inf')], 
+    labels=["Left", "Right"]
+)
 
-# Capitalize first letter 
-df['type_1'] = df['type_1'].str.capitalize()  
-
-# long format
-df_long = df.melt(id_vars=['pokemon', 'type_1'], value_vars=stats, var_name='stat', value_name='value')
-
-df_long['stat'] = df_long['stat'].str.capitalize()
+df_clean['quadrant'] = pd.Series([
+    'Bottom-Left' if (row['inj'] <= line_1 and row['fat'] <= line_2) else 
+    'Bottom-Right' if (row['inj'] > line_1 and row['fat'] <= line_2) else 
+    'Top-Left' if (row['inj'] <= line_1 and row['fat'] > line_2) else 
+    'Top-Right' for _, row in df_clean.iterrows()
+])
 
 
 # plot --------
 
 g = (
-    ggplot(df_long)
+    ggplot(df_clean) +
 
-    + aes(x='value', fill='type_1')
-
-    # + coord_flip()
+    aes(x = 'inj', y = 'fat', color='quadrant') +
     
-    + geom_histogram(binwidth = 10, alpha = 0.85, color = 'white', size = 0.15)
+    geom_point(alpha = 0.85, size = 3) +
 
-    + facet_grid('stat~type_1')  # One row per stat, one column per type
+    # + facet_grid('stat~type_1')  # One row per stat, one column per type
 
-    + scale_fill_manual(values = col)
+    # + scale_fill_manual(values = col)
 
-    + theme_minimal()
+    # Add lines to separate the plot into 4 quadrants
+    geom_vline(xintercept = line_1, color = "#7B726F", linetype = "dashed", size = 0.5) + # Vertical line at median injuries
+    geom_hline(yintercept = line_2, color = "#7B726F", linetype = "dashed", size = 0.5) + # Horizontal line at median fatalities
+
+    scale_color_manual(values={"Bottom-Left": "#6F99AD", "Bottom-Right": "#D78D50", "Top-Left": "#BC3C29", "Top-Right": "#6f6e9a"}) + # Custom colors for each quadrant
+    theme_minimal(base_family = "Candara") +
     
-    + labs(
-        title="Pokémon Stat Distributions by Type",
-        subtitle="Histograms of key stats (Attack, Defense, Speed) for Electric, Fire, Grass, and Water types",
-        caption="Source: {pokemon} R package | Graphic: Natasa Anastasiadou",
-        x = 'Pokémon Stat Score',
-        y = 'Number of Pokémon'
-    )
-    + theme(
+    labs(
+        title="Tornado Injuries vs Fatalities",
+        # subtitle="Histograms of key stats (Attack, Defense, Speed) for Electric, Fire, Grass, and Water types",
+        # caption="Source: {pokemon} R package | Graphic: Natasa Anastasiadou",
+        x = 'No of Injuries',
+        y = 'No of Fatalities'
+    ) +
+
+    theme(
         legend_position = 'none',
         
-        axis_text = element_text(family = 'Candara', size = 8),
-        axis_title = element_text(family = 'Candara', size = 9),
+        # axis_text = element_text(family = 'Candara', size = 8),
+        # axis_title = element_text(family = 'Candara', size = 9),
 
-        plot_title = element_text(size = 12, weight='bold', family='Candara', ha='center'),
-        plot_subtitle = element_text(size = 10, family='Candara', ha='center'),
-        plot_caption = element_text(size = 7, family='Candara', ha='right'),
+        # plot_title = element_text(size = 12, weight='bold', family='Candara', ha='center'),
+        # plot_subtitle = element_text(size = 10, family='Candara', ha='center'),
+        # plot_caption = element_text(size = 7, family='Candara', ha='right'),
         
-        strip_text = element_text(size = 8, family='Candara'),
+        # strip_text = element_text(size = 8, family='Candara'),
         
-        panel_grid_major = element_line(color = '#e5e5e5', alpha = 0.75, size = 0.65, linetype = "dashed"),
-        # panel_grid_major = element_blank(),
+        panel_grid_major = element_line(color = '#c9c9c9', alpha = 0.75, size = 0.65, linetype = "dashed"),
         panel_grid_minor = element_blank(),
 
-        panel_border = element_rect(color = '#e5e5e5', alpha = 0.7, size = 0.5, fill = None),
+        # panel_border = element_rect(color = '#e5e5e5', alpha = 0.7, size = 0.5, fill = None),
         
-        plot_background = element_rect(fill = 'white', color = 'white'),
-        panel_background = element_rect(fill = 'white', color = 'white'),
+        plot_background = element_rect(fill = '#e9e9e9', color = '#e9e9e9'),
+        panel_background = element_rect(fill = '#e9e9e9', color = '#e9e9e9'),
         
-        axis_ticks= element_blank(),
+        # axis_ticks= element_blank(),
         
         figure_size = (10, 6)
     )
