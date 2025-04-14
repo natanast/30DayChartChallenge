7484925894ca1b5a83654817fd1d851b2ac5e560
NatAnastas <anastasiadounat@gmail.com>
2025-04-14 15:34:42 +0300

    tree

--- 2025/14/14_day.R
+++ 2025/14/14_day.R
@@ -111,45 +111,21 @@ ggsave(
 
 
 
-
-library(ggplot2)
-
-# Define the 4 people and their positions
+# Extend the people dataframe with Chandler's family
 people <- data.frame(
-    name = c("Carol", "Ross", "Monica", "Chandler"),
-    x = c(1, 2, 3, 4),
-    y = c(1, 1, 1, 1)
+    name = c("Leonard", "Sandra", "Jill", "Amy", "Rachel", 
+             "Jack", "Judy", "Ross", "Monica", 
+             "Charles", "Nora", "Chandler"),
+    x = c(-2, -1, -2, -1, 0, 
+          1, 2, 1, 2, 
+          3, 4, 3.5),
+    y = c(2, 2, 1, 1, 1, 
+          2, 2, 1, 1, 
+          2, 2, 1)
 )
 
-# # Relationship text annotations
-# annotations <- data.frame(
-#     label = c("Ex-Wife", "Siblings", "Married"),
-#     x = c(1.5, 2.5, 3.5),
-#     y = c(1.2, 1.2, 1.2)
-# )
-
-# Plot with points, names, and relationship labels
+# Plot with Chandler's family added
 ggplot() +
-    geom_point(data = people, aes(x = x, y = y), size = 5, color = "steelblue") +
-    geom_text(vjust = -1.2, size = 5) +
-    # geom_text(data = annotations, aes(x = x, y = y, label = label), size = 4, color = "darkred") +
-    theme_void() +
-    coord_fixed() 
-    # ggtitle("Friends Kinship Snippet: Carol – Ross – Monica – Chandler")
-
-library(ggplot2)
-
-# Define the people and their positions
-people <- data.frame(
-    name = c("Leonard", "Sandra", "Jill", "Amy", "Rachel", "Jack", "Judy", "Ross", "Monica"),
-    x = c(-2, -1, -2, -1, 0, 1, 2, 1, 2),
-    y = c(2, 2, 1, 1, 1, 2, 2, 1, 1)
-)
-
-
-# Plot
-ggplot() +
-    
     geom_point(data = people, aes(x = x, y = y), size = 6) +
     geom_text(data = people, aes(x = x, y = y, label = name), vjust = -1) +
     
@@ -161,17 +137,19 @@ ggplot() +
     geom_segment(aes(x = -2, xend = -2, y = 1, yend = 1.5)) +
     geom_segment(aes(x = -1, xend = -1, y = 1, yend = 1.5)) +
     geom_segment(aes(x = 0, xend = 0, y = 1, yend = 1.5)) +
-
+    geom_segment(aes(x = 0, xend = 1, y = 1, yend = 1)) +
+    
     # Gellers
     geom_segment(aes(x = 1, xend = 2, y = 2, yend = 2)) +
     geom_segment(aes(x = 1.5, xend = 1.5, y = 1.5, yend = 2)) +
-    geom_segment(aes(x = 1, xend = 1.5, y = 1.5, yend = 1.5)) +
-    geom_segment(aes(x = 1.5, xend = 2, y = 1.5, yend = 1.5)) +
+    geom_segment(aes(x = 1, xend = 2, y = 1.5, yend = 1.5)) +
     geom_segment(aes(x = 1, xend = 1, y = 1, yend = 1.5)) +
     geom_segment(aes(x = 2, xend = 2, y = 1, yend = 1.5)) +
     
-    # geom_segment(aes(x = 1, xend = 2, y = 1, yend = 1)) +
+    # Bings
+    geom_segment(aes(x = 3, xend = 4, y = 2, yend = 2)) +
+    geom_segment(aes(x = 3.5, xend = 3.5, y = 1.5, yend = 2)) +
+    geom_segment(aes(x = 2, xend = 3.5, y = 1, yend = 1)) +
+    geom_segment(aes(x = 3.5, xend = 3.5, y = 1, yend = 1.5)) +
     
-    theme_minimal() 
-
-
+    theme_minimal()
