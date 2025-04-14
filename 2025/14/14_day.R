38120fbc4cd4512866573d1fc6f5b3df4a748273
NatAnastas <anastasiadounat@gmail.com>
2025-04-14 19:24:21 +0300

    shape

--- 2025/14/14_day.R
+++ 2025/14/14_day.R
@@ -198,86 +198,116 @@ people[, sex := c(
     "M", "M", "F", "F",
     # Phoebe’s side
     "F", "M", "F", "M", "F",
-    "F", "F", "F",
+    "F", "M", "F",
     "M", "F", "F",
     # Tribbianis
     "M", "M", "F", "F"
 )]
 
 
-
-
+col = c("#00429d", "#b24745")
 
 # plot -----------
 
-ggplot() +
-    geom_point(data = people, aes(x = x, y = y, color = sex), size = 4) +
-    geom_text(data = people, aes(x = x, y = y, label = name), vjust = -1, size = 3) +
-    
+gr = ggplot() +
+
     # Greens
-    geom_segment(aes(x = -2, xend = -1, y = 2, yend = 2)) +
-    geom_segment(aes(x = -1.5, xend = -1.5, y = 1.5, yend = 2)) +
-    geom_segment(aes(x = -1.5, xend = -2, y = 1.5, yend = 1.5)) +
-    geom_segment(aes(x = -1.5, xend = 0, y = 1.5, yend = 1.5)) +
-    geom_segment(aes(x = -2, xend = -2, y = 1, yend = 1.5)) +
-    geom_segment(aes(x = -1, xend = -1, y = 1, yend = 1.5)) +
-    geom_segment(aes(x = 0, xend = 0, y = 1, yend = 1.5)) +
+    geom_segment(aes(x = -2, xend = -1, y = 2, yend = 2), color = "grey75") +
+    geom_segment(aes(x = -1.5, xend = -1.5, y = 1.5, yend = 2), color = "grey75") +
+    geom_segment(aes(x = -1.5, xend = -2, y = 1.5, yend = 1.5), color = "grey75") +
+    geom_segment(aes(x = -1.5, xend = 0, y = 1.5, yend = 1.5), color = "grey75") +
+    geom_segment(aes(x = -2, xend = -2, y = 1, yend = 1.5), color = "grey75") +
+    geom_segment(aes(x = -1, xend = -1, y = 1, yend = 1.5), color = "grey75") +
+    geom_segment(aes(x = 0, xend = 0, y = 1, yend = 1.5), color = "grey75") +
     
     # Ross & Rachel
-    geom_segment(aes(x = 0, xend = 1, y = 1, yend = 1)) +
-    geom_segment(aes(x = 0.25, xend = 0.25, y = 0, yend = 1)) +
+    geom_segment(aes(x = 0, xend = 1, y = 1, yend = 1), color = "grey75") +
+    geom_segment(aes(x = 0.25, xend = 0.25, y = 0, yend = 1), color = "grey75") +
     
     # Gellers
-    geom_segment(aes(x = 1, xend = 2, y = 2, yend = 2)) +
-    geom_segment(aes(x = 1.5, xend = 1.5, y = 1.5, yend = 2)) +
-    geom_segment(aes(x = 0.5, xend = 2, y = 1.5, yend = 1.5)) +
-    geom_segment(aes(x = 0.5, xend = 0.5, y = 1, yend = 1.5)) +
-    geom_segment(aes(x = 2, xend = 2, y = 1, yend = 1.5)) +
+    geom_segment(aes(x = 1, xend = 2, y = 2, yend = 2), color = "grey75") +
+    geom_segment(aes(x = 1.5, xend = 1.5, y = 1.5, yend = 2), color = "grey75") +
+    geom_segment(aes(x = 0.5, xend = 2, y = 1.5, yend = 1.5), color = "grey75") +
+    geom_segment(aes(x = 0.5, xend = 0.5, y = 1, yend = 1.5), color = "grey75") +
+    geom_segment(aes(x = 2, xend = 2, y = 1, yend = 1.5), color = "grey75") +
     
     # Bings
-    geom_segment(aes(x = 3, xend = 4, y = 2, yend = 2)) +
-    geom_segment(aes(x = 3.5, xend = 3.5, y = 1.5, yend = 2)) +
-    geom_segment(aes(x = 2, xend = 3.5, y = 1, yend = 1)) +
-    geom_segment(aes(x = 3.5, xend = 3.5, y = 1, yend = 1.5)) +
+    geom_segment(aes(x = 3, xend = 4, y = 2, yend = 2), color = "grey75") +
+    geom_segment(aes(x = 3.5, xend = 3.5, y = 1.5, yend = 2), color = "grey75") +
+    geom_segment(aes(x = 2, xend = 3.5, y = 1, yend = 1), color = "grey75") +
+    geom_segment(aes(x = 3.5, xend = 3.5, y = 1, yend = 1.5), color = "grey75") +
     
     # Ross & Carol
-    geom_segment(aes(x = 0.5, xend = 1, y = 1, yend = 1)) +
-    geom_segment(aes(x = 0.65, xend = 0.75, y = 0.98, yend = 1.02), linewidth = .8) +
-    geom_segment(aes(x = 0.75, xend = 0.85, y = 0.98, yend = 1.02),linewidth = .8) +
-    geom_segment(aes(x = 0.75, xend = 0.75, y = 0, yend = 1)) +
+    geom_segment(aes(x = 0.5, xend = 1, y = 1, yend = 1), color = "grey75") +
+    geom_segment(aes(x = 0.65, xend = 0.75, y = 0.98, yend = 1.02), linewidth = .8, color = "grey75") +
+    geom_segment(aes(x = 0.75, xend = 0.85, y = 0.98, yend = 1.02),linewidth = .8, color = "grey75") +
+    geom_segment(aes(x = 0.75, xend = 0.75, y = 0, yend = 1), color = "grey75") +
     
     # Buffays: Frank & Lily -> Phoebe & Ursula
-    geom_segment(aes(x = 6, xend = 7, y = 2, yend = 2)) +  # Frank Sr. + Lily
-    geom_segment(aes(x = 6.5, xend = 6.5, y = 1.5, yend = 2)) +  # Down to children
-    geom_segment(aes(x = 5.3, xend = 7, y = 1.5, yend = 1.5)) +  # Horizontal to connect kids
-    geom_segment(aes(x = 6, xend = 6, y = 1, yend = 1.5)) +  # Ursula
-    geom_segment(aes(x = 7, xend = 7, y = 1, yend = 1.5)) +  # Phoebe
-    geom_segment(aes(x = 5.3, xend = 5.3, y = 1, yend = 1.5)) + 
+    geom_segment(aes(x = 6, xend = 7, y = 2, yend = 2), color = "grey75") +  # Frank Sr. + Lily
+    geom_segment(aes(x = 6.5, xend = 6.5, y = 1.5, yend = 2), color = "grey75") +  # Down to children
+    geom_segment(aes(x = 5.3, xend = 7, y = 1.5, yend = 1.5), color = "grey75") +  # Horizontal to connect kids
+    geom_segment(aes(x = 6, xend = 6, y = 1, yend = 1.5), color = "grey75") +  # Ursula
+    geom_segment(aes(x = 7, xend = 7, y = 1, yend = 1.5), color = "grey75") +  # Phoebe
+    geom_segment(aes(x = 5.3, xend = 5.3, y = 1, yend = 1.5), color = "grey75") + 
     
     # Phoebe + Mike
-    geom_segment(aes(x = 7, xend = 8, y = 1, yend = 1)) +
+    geom_segment(aes(x = 7, xend = 8, y = 1, yend = 1), color = "grey75") +
     
     # Frank Jr + Alice
-    geom_segment(aes(x = 4.5, xend = 5.3, y = 1, yend = 1)) +
+    geom_segment(aes(x = 4.5, xend = 5.3, y = 1, yend = 1), color = "grey75") +
     
     # triplets
-    geom_segment(aes(x = 5, xend = 5, y = 0.5, yend = 1)) +
-    geom_segment(aes(x = 4.5, xend = 5.5, y = 0.5, yend = 0.5)) +
-    geom_segment(aes(x = 4.5, xend = 4.5, y = 0, yend = 0.5)) +
-    geom_segment(aes(x = 5, xend = 5, y = 0, yend = 0.5)) +
-    geom_segment(aes(x = 5.5, xend = 5.5, y = 0, yend = 0.5)) +
+    geom_segment(aes(x = 5, xend = 5, y = 0.5, yend = 1), color = "grey75") +
+    geom_segment(aes(x = 4.5, xend = 5.5, y = 0.5, yend = 0.5), color = "grey75") +
+    geom_segment(aes(x = 4.5, xend = 4.5, y = 0, yend = 0.5), color = "grey75") +
+    geom_segment(aes(x = 5, xend = 5, y = 0, yend = 0.5), color = "grey75") +
+    geom_segment(aes(x = 5.5, xend = 5.5, y = 0, yend = 0.5), color = "grey75") +
     
     # susan carol
-    geom_segment(aes(x = 0.75, xend = 1.35, y = 1, yend = 1)) +
+    geom_segment(aes(x = 0.75, xend = 1.35, y = 1, yend = 1), color = "grey75") +
     
     #tribianni
-    geom_segment(aes(x = 9, xend = 10, y = 2, yend = 2)) +
-    geom_segment(aes(x = 9.5, xend = 9.5, y = 1.5, yend = 2)) +
-    geom_segment(aes(x = 9, xend = 9.5, y = 1.5, yend = 1.5)) +
-    geom_segment(aes(x = 9.5, xend = 10, y = 1.5, yend = 1.5)) +
-    geom_segment(aes(x = 9, xend = 9, y = 1, yend = 1.5)) +
-    geom_segment(aes(x = 10, xend = 10, y = 1, yend = 1.5)) +
+    geom_segment(aes(x = 9, xend = 10, y = 2, yend = 2), color = "grey75") +
+    geom_segment(aes(x = 9.5, xend = 9.5, y = 1.5, yend = 2), color = "grey75") +
+    geom_segment(aes(x = 9, xend = 9.5, y = 1.5, yend = 1.5), color = "grey75") +
+    geom_segment(aes(x = 9.5, xend = 10, y = 1.5, yend = 1.5), color = "grey75") +
+    geom_segment(aes(x = 9, xend = 9, y = 1, yend = 1.5), color = "grey75") +
+    geom_segment(aes(x = 10, xend = 10, y = 1, yend = 1.5), color = "grey75") +
     
-    theme_minimal()
+    geom_text(data = people, aes(x = x, y = y, label = name), vjust = -1, size = 3) +
+    
+    geom_point(data = people, aes(x = x, y = y, color = sex), size = 4,
+               shape = ifelse(people$sex == "M", 15, 16)) +
+    
+    scale_color_manual(values = col) +
+    
+    theme_minimal() +
+    
+    theme(
+        legend.position = "none",  
+        
+        plot.title = element_markdown(size = 14, face = "bold", hjust = .25, margin = margin(b = 5, t = 5)),
+        plot.subtitle = element_markdown(size = 10, hjust = 0.3, color = "grey30", margin = margin(b = 15, t = 5)),
+        plot.caption = element_markdown(size = 8, hjust = 1, margin = margin(t = 10)),
+        
+        axis.text = element_blank(),
+        axis.title = element_blank(),
+        
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        
+        plot.margin = margin(20, 20, 20, 20),
+        plot.background = element_rect(fill = "grey93", color = NA)
+    )
+
+
+# save ---------
+
+ggsave(
+    plot = gr, filename = "Rplot.png",
+    width = 14, height = 8, units = "in", dpi = 600
+)
+
 
 
