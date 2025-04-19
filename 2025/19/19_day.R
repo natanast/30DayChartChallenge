d4123d5947c5c4bbd930fe48f7e9a9e122ebd50b
NatAnastas <anastasiadounat@gmail.com>
2025-04-19 14:44:42 +0300

    p

--- 2025/19/19_day.R
+++ 2025/19/19_day.R
@@ -13,12 +13,17 @@ library(gghighlight)
 library(ggtext)
 library(extrafont)
 
-# Load data ---------
+library(grid)
+
+
+# Load data -------
+
 big_tech_stock_prices <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2023/2023-02-07/big_tech_stock_prices.csv')
 big_tech_companies <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2023/2023-02-07/big_tech_companies.csv')
 
 
-# Clean data
+# Clean data ------
+
 big_tech_stock_prices$date <- as.Date(big_tech_stock_prices$date)
 
 # Extract year
@@ -26,24 +31,24 @@ big_tech_stock_prices$year <- format(big_tech_stock_prices$date, "%Y")
 
 big_tech_stock_prices <- big_tech_stock_prices[year != "2023"]
 
-# Convert data to a data.table
-setDT(big_tech_stock_prices)
+# # Convert data to a data.table
+# setDT(big_tech_stock_prices)
 
 # Calculate yearly average adjusted close price
 yearly_avg <- big_tech_stock_prices[, .(adj_close = mean(adj_close, na.rm = TRUE)), by = .(stock_symbol, year)]
 
 
-df <- merge(yearly_avg, big_tech_companies, by = "stock_symbol", all.x = TRUE)
 
+
+df <- merge(yearly_avg, big_tech_companies, by = "stock_symbol", all.x = TRUE)
 
 df$company <- str_replace_all(df$company, " Inc\\.|,", "")
 
-df$company <- ifelse(df$company == "International Business Machines Corporation", df$stock_symbol, df$company)
+df$company <- df$company |> str_wrap(width = 25)
 
 
-# Filter data for the year 2022
+
 df_2022 <- df[df$year == 2022, ]
-
 
 
 # plot -------
@@ -62,10 +67,8 @@ g <- ggplot(df, aes(x = as.numeric(year), y = adj_close, color = company)) +
     geom_point(data = df_2022, aes(x = as.numeric(year), y = adj_close, fill = company), 
                size = 2.5, shape = 21, stroke = 0.1, color = "white") +  
     
-
-    
     gghighlight(use_direct_label = FALSE,
-                unhighlighted_params = list(colour = alpha("grey80", 1))) +
+                unhighlighted_params = list(colour = alpha("grey83", 1))) +
     
     geom_text(
         data = df_2022,
@@ -83,6 +86,7 @@ g <- ggplot(df, aes(x = as.numeric(year), y = adj_close, color = company)) +
     
     facet_wrap('~company', ncol = 7) +
     
+
     scale_x_continuous( breaks = c(2010, 2016, 2022)) +
     
     scale_color_manual(values = col) +
@@ -90,8 +94,8 @@ g <- ggplot(df, aes(x = as.numeric(year), y = adj_close, color = company)) +
     scale_fill_manual(values = col) +
     
     labs(
-        title = "Big Tech Stock Trends Over Time",
-        subtitle = "Each facet highlights one company",
+        title = "Stock Trends of Big Tech Companies Over the Years",
+        subtitle = "Tracking the Annual Adjusted Closing Prices of Leading Tech Giants from 2010 to 2022",
         caption = "30DayChartChallenge 2025: <b> Day 19</b> | Source: <b> Big Tech Stock Prices (Kaggle) </b> | Graphic: <b>Natasa Anastasiadou</b>",
         x = "",
         y = "Avg Adj Close Price"
@@ -104,7 +108,7 @@ g <- ggplot(df, aes(x = as.numeric(year), y = adj_close, color = company)) +
         
         plot.title = element_markdown(size = 11, face = "bold", color = "grey20", hjust = 0.5, family = "Candara", margin = margin(t = 2, b = 5)),
         plot.subtitle = element_markdown(size = 9, hjust = 0.5, color = "grey40", margin = margin(t = 5, b = 20)),
-        plot.caption = element_markdown(margin = margin(t = 5), size = 7, hjust = 1),
+        plot.caption = element_markdown(margin = margin(t = 5), size = 5.5, hjust = 1),
         
         panel.grid.major = element_line(linewidth = .25, color = "grey80", linetype = "dashed", lineend = "round"),
         panel.grid.minor = element_line(linewidth = .25, color = "grey80", linetype = "dashed", lineend = "round"),
@@ -119,7 +123,7 @@ g <- ggplot(df, aes(x = as.numeric(year), y = adj_close, color = company)) +
         plot.background = element_rect(fill = "#e4e4e3", color = NA),
         
         plot.margin = margin(20, 20, 20, 20)
-    )
+    ) 
 
 g
 
