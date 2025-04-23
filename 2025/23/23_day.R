c67c6f020ad8d51af9fef7bdc9910aca2dfbb896
NatAnastas <anastasiadounat@gmail.com>
2025-04-23 19:17:35 +0300

    23

--- 2025/23/23_day.R
+++ 2025/23/23_day.R
@@ -32,14 +32,15 @@ big_tech_filtered <- big_tech_stock_prices[stock_symbol %in% selected]
 stock_yearly_avg <- big_tech_filtered[, .(avg_close = mean(close, na.rm = TRUE)), by = .(stock_symbol, year)]
 
 
-# col = c("#00429d", "#73a2c6", '#396375', '#5a8192', '#6f6e9a', '#a2a0cf', '#e37b78',"#A65628", '#b24745')
 
 
 
-col = c('#5a8192', '#b24745','#a2a0cf', "#00429d" )
 
 
 # plot -----------
+
+col = c('#5a8192', '#b24745','#a2a0cf', "#00429d" )
+
 
 p = ggplot(stock_yearly_avg, aes(x = year, y = avg_close)) +
     
@@ -54,7 +55,14 @@ p = ggplot(stock_yearly_avg, aes(x = year, y = avg_close)) +
     
     scale_y_log10() +
     
-    scale_color_manual(values = col) +
+    scale_x_continuous(
+        breaks = c(2010, 2012, 2014, 2016, 2018, 2020, 2022)
+    ) +
+    
+    scale_color_manual(
+        values = col,
+        name = "Companies"
+    ) +
     
     # labs(
     #     title    = "Netflix Monthly Avg Closing Price",
@@ -64,40 +72,37 @@ p = ggplot(stock_yearly_avg, aes(x = year, y = avg_close)) +
     # ) +
     # 
     # 
-    # labs(
-    #     title = "Media & Time: Tracking Section Popularity by Year",
-    #     subtitle = "Top 5 Guardian news sections published each year from 2016 to 2021.",
-    #     caption = "30DayChartChallenge: <b> Day 18</b> | Source: <b> Guardian News Articles (Kaggle) </b> | Graphic: <b>Natasa Anastasiadou</b>",
-    #     y = "No. of Article", 
-    #     fill = "Type"
-    # ) +
-    # 
+    labs(
+        title = "Yearly Average Closing Prices of Big Tech Stocks",
+        subtitle = "Top 5 Guardian news sections published each year from 2016 to 2021.",
+        caption = "30DayChartChallenge 2025: <b> Day 23</b> | Source: <b> Big Tech Stock Prices (TidyTuesday) </b> | Graphic: <b>Natasa Anastasiadou</b>",
+        y = "Avg Closing Price (log scale)",
+        fill = "Type"
+    ) +
+
     theme_minimal(base_family = "Candara") +
     
     theme(
         
         axis.title.x = element_blank(),
         
-        axis.title.y = element_text(size = 9),
+        axis.title.y = element_text(size = 12),
         
-        axis.text.x = element_text(size = 7),
-        axis.text.y = element_text(size = 7),
+        axis.text = element_text(size = 10),
         
-        legend.text = element_text(size = 7),         # Smaller text
-        legend.title = element_text(size = 8),        # Optional: smaller title
-        legend.key.size = unit(0.8, "lines"),         # Smaller boxes
+        legend.position = c(.85, .28),
+        legend.text = element_text(size = 9),         
+        legend.title = element_text(size = 10),        
+        legend.key.size = unit(0.8, "lines"),
         legend.spacing.y = unit(0.5, "lines"),
         
-        legend.position = "right",
-        # legend.title = element_text(size = 8),
-        # legend.text = element_text(size = 7),
-        
+
         panel.grid.major = element_line(color = "grey75", linetype = "dashed", lineend = "round"),
         panel.grid.minor = element_blank(),
         
         plot.title = element_markdown(size = 14, face = "bold", hjust = 0.5, margin = margin(t = 2, b = 2)),
         plot.subtitle = element_markdown(size = 10, hjust = 0.5,  color = "grey30", margin = margin(t = 5, b = 10)),
-        plot.caption  = element_markdown(margin = margin(t = 25), size = 6, hjust = 1.38),
+        plot.caption  = element_markdown(margin = margin(t = 25), size = 6, hjust = 1),
         
         
         plot.margin = margin(20, 20, 20, 20),
