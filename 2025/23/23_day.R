8a7cd0a7e3a76b4f24bd35b1baeec71ffabe0acf
NatAnastas <anastasiadounat@gmail.com>
2025-04-23 14:51:33 +0300

    23

--- 2025/23/23_day.R
+++ 2025/23/23_day.R
@@ -4,184 +4,124 @@ rm(list = ls())
 gc()
 
 
-# libraries ---------
+# load libraries -------
 
 library(data.table)
+library(ggplot2)
 library(stringr)
-library(ggplot2)
-library(ggrepel)
-library(colorspace)
-library(ggplot2)
-library(shadowtext)
-
-library(airway)
-library(DESeq2)
-
-library(genekitr)
-
 library(ggtext)
 library(extrafont)
-library(showtext)
-# library(geneset)
 
 
-# Load showtext and register Candara
-# library(showtext)
-# font_add("Candara", regular = "C:/Windows/Fonts/candara.ttf")  # Path to Candara font
-# showtext_auto()
+# load data --------
+
+news <- fread('guardian_articles.csv')
 
 
-# Load data -------
+# data cleaning -----------
 
-data("airway")
-airway$dex <- relevel(airway$dex, ref = "untrt")  
-
-
-# Data analysis -----
-
-dds <- DESeqDataSet(airway, design = ~ dex)
+df <- news[, .(sectionName, webPublicationDate)]
 
 
-dds <- DESeq(dds)
+df$webPublicationDate <-  df$webPublicationDate |> str_extract("^\\d{4}")
 
-res <- results(dds) |>
-    as.data.frame() |>
-    setDT(keep.rownames = "GeneID")
+# Remove rows where sectionName is NA or empty
+df <- df[!is.na(sectionName) & sectionName != ""]
 
-
-df = res[which( !is.na(res$pvalue) )]
-
-df$y = -log10(df$pvalue)
+# Remove 2022
+df <- df[webPublicationDate != "2022"]
 
 
-
-gene_map = df$GeneID |> transId(transTo = c("symbol", "entrez", "ensembl"), unique = TRUE) |> setDT()
-
-gene_map = gene_map[which(
-    !is.na(symbol) & !is.na(entrezid) & !is.na(ensembl)
-)]
-
-df = df |> merge(gene_map, by.x = "GeneID", by.y = "input_id")
-
-df = df[!is.na(padj)]
+count <- df[, .N, by = .(webPublicationDate, sectionName)]
 
 
-df$ann = ifelse(
-    df$pvalue > .05, "Not significant",
-    ifelse(
-        df$log2FoldChange > 0, "Up regulated", "Down regulated"
-    )
-)
+# For each year, keep the top 5 sections based on the count
+top_5_sections <- count[, head(.SD[order(-N)]), by = webPublicationDate]
+
+top_5_sections <- top_5_sections[, .SD[1:5], by = webPublicationDate]
 
 
-df$ann = ifelse(
-    df$pvalue <= 0.05 & df$log2FoldChange > -1 & df$log2FoldChange < 1, 
-    paste0(df$ann, " (low)"),
-    df$ann
+# plot -----------
+
+
+col = c("#00429d", "#73a2c6", '#396375', '#5a8192', '#6f6e9a', '#a2a0cf', '#e37b78',"#A65628", '#b24745')
+
+
+grid_colors = c(
+    "American Crow"             = '#6f6e9a',
+    "American Goldfinch"        = "#A65628",
+    "American Robin"            = "#b24745",
+    "Belted Kingfisher"         = "#00429d",
+    "Blue Jay"                  = "#396375",
+    "Downy Woodpecker"          = '#a2a0cf',
+    "Green Heron"               = "#FDAE61",
+    "Northern Flicker"          = "#e37b78",
+    "Ruby-throated Hummingbird" = "#73a2c6", 
+    "Scarlet Tanager"           = "#7f9faa"
 )
 
 
 
-df2 = df[which(pvalue <= .05 & abs(log2FoldChange) > 1)]
+# plot ---------
 
-df2 = df2[order( abs(log2FoldChange), decreasing = TRUE )]
-
-df2 = df2[, by = ann, head(.SD, 10) ]
-
-
-
-# plot -----
-
-gr = ggplot(data = df) +
+# Create the stacked bar plot
+p = ggplot(top_5_sections, aes(x = webPublicationDate, y = N, fill = sectionName)) +
     
-    geom_point(aes(x = log2FoldChange, y = -log10(pvalue), fill = ann),
-               shape = 21, stroke = .05, size = 2, alpha = .5, color = "white") +
+    geom_bar(stat = "identity", width = 0.5) +
     
-    geom_vline(xintercept = c(-1, 1), linewidth = .3, linetype = "dashed", lineend = "round") +
-    
-    geom_hline(yintercept = -log10(.05), linewidth = .3, linetype = "dashed", lineend = "round") +
-    
-    geom_point(data = df2, aes(x = log2FoldChange, y = -log10(pvalue), fill = ann), 
-               shape = 21, stroke = .2, size = 2.5, color = "white") +
-    
-    geom_text_repel(
-        data = df2, aes(x = log2FoldChange, y = -log10(pvalue), label = symbol),
-        max.overlaps = Inf, 
-        fontface = "bold", size = 13, bg.color = "white", bg.r = .05
-    ) +
-    
-    scale_fill_manual(
-        values = c(
-            "Up regulated" = "#990000",
-            "Up regulated (low)" = lighten("#990000", .5),
-            
-            "Down regulated" = "#004d99",
-            "Down regulated (low)" = lighten("#004d99", .5),
-            
-            "Not significant" = "grey"
-        ),
-        
-        breaks = c("Up regulated", "Not significant", "Down regulated"),
-        
-        guide = guide_legend(
-            override.aes = list(size = 3, alpha = 1)
-        ),
-        
-    ) +
-    
-    scale_x_continuous(
-        breaks = c(-5, -2.5, -1, 0, 1, 2.5, 5),
-        trans = scales::pseudo_log_trans()
-    ) +
-    
-    scale_y_continuous(
-        expand = c(0, 0), breaks = c(2, 5, 10, 20, 30, 40),
-        trans = scales::pseudo_log_trans()
-    ) +
-    
-    coord_cartesian(clip = "off") +
+    scale_fill_manual(values = col) +
     
     labs(
-        y = "-log10(pvalue)", 
-        x = "log2(Fold Change)", 
-        title = "Glucocorticoid Response in Airway Cells",
-        subtitle = "Differential gene expression in human airway smooth muscle cells treated with dexamethasone vs. untreated control",
-        caption = "30DayChartChallenge: <b> Day 23</b> | Source: <b> {airway} R package </b> | Graphic: <b>Natasa Anastasiadou</b>",
+        title = "Media & Time: Tracking Section Popularity by Year",
+        subtitle = "Top 5 Guardian news sections published each year from 2016 to 2021.",
+        caption = "30DayChartChallenge: <b> Day 18</b> | Source: <b> Guardian News Articles (Kaggle) </b> | Graphic: <b>Natasa Anastasiadou</b>",
+        y = "No. of Article", 
+        fill = "Type"
     ) +
     
     theme_minimal(base_family = "Candara") +
     
     theme(
-        legend.title = element_blank(),
-        legend.position = "bottom",
         
-        legend.text = element_text(size = 55),
+        axis.title.x = element_blank(),
         
-        plot.title = element_markdown(size = 85, face = "bold", hjust = 0.5, margin = margin(t = 2, b = 2)),
-        plot.subtitle = element_markdown(size = 75, hjust = 0.5,  color = "grey30", margin = margin(t = 5, b = 20)),
-        plot.caption  = element_markdown(margin = margin(t = 25), size = 50, hjust = 1),
+        axis.title.y = element_text(size = 9),
         
+        axis.text.x = element_text(size = 7),
+        axis.text.y = element_text(size = 7),
         
-        axis.title = element_text(size = 55),
-        axis.text = element_text(size = 55),
+        legend.text = element_text(size = 7),         # Smaller text
+        legend.title = element_text(size = 8),        # Optional: smaller title
+        legend.key.size = unit(0.8, "lines"),         # Smaller boxes
+        legend.spacing.y = unit(0.5, "lines"),
         
-        axis.line = element_line(linewidth = .3, color = "black"),
-        axis.ticks = element_line(linewidth = .3, color = "black"),
+        legend.position = "right",
+        # legend.title = element_text(size = 8),
+        # legend.text = element_text(size = 7),
         
+        panel.grid.major = element_line(color = "grey75", linetype = "dashed", lineend = "round"),
         panel.grid.minor = element_blank(),
-        panel.grid.major = element_line(linewidth = .25, linetype = "dashed", lineend = "round", color = "grey80"),
         
-        plot.background = element_rect(fill = "grey98", color = NA),
+        plot.title = element_markdown(size = 14, face = "bold", hjust = 0.5, margin = margin(t = 2, b = 2)),
+        plot.subtitle = element_markdown(size = 10, hjust = 0.5,  color = "grey30", margin = margin(t = 5, b = 10)),
+        plot.caption  = element_markdown(margin = margin(t = 25), size = 6, hjust = 1.38),
+        
         
         plot.margin = margin(20, 20, 20, 20),
         
+        plot.background = element_rect(fill = "#e4e4e3", color = NA)
         
     )
 
 
-gr
+
+p
 
 
-# Save the plot
-ggsave("23_day.png", plot = gr, width = 9, height = 9, dpi = 600)
+ggsave(
+    plot = p, filename = "Rplot.png",
+    width = 8, height = 7, units = "in", dpi = 600
+)    
+
+
 
