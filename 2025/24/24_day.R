cec742482dc3f0914c4d9e4e687e63bb91f3f805
NatAnastas <anastasiadounat@gmail.com>
2025-04-24 10:34:21 +0300

    24

--- 2025/24/24_day.R
+++ 2025/24/24_day.R
@@ -61,7 +61,9 @@ ggplot(df_plot, aes(x = Year, y = LE_birth_both_sexes, group = 1)) +
     labs(
         title = "Life Expectancy at Birth by Income Group (2000–2019)",
         subtitle = "Lollipop timeseries by WHO",
-        caption = "30DayChartChallenge 2025: <b> Day 23</b> | Source: <b> Big Tech Stock Prices (TidyTuesday) </b> | Graphic: <b>Natasa Anastasiadou</b>",
+        caption = "30DayChartChallenge 2025: <b> Day 24</b> 
+                   | Source: <b> Life Expectancy WHO data (Kaggle) </b> 
+                   | Graphic: <b>Natasa Anastasiadou</b>",
         y = "Life Expectancy (Years)",
         x = "Year"
         # fill = "Type"
@@ -72,87 +74,24 @@ ggplot(df_plot, aes(x = Year, y = LE_birth_both_sexes, group = 1)) +
     theme(
         # strip.text = element_text(face = "bold", size = 14),
         axis.text.x = element_text(angle = 45, hjust = 1),
+
+        panel.grid.major = element_line(color = "grey85", linewidth = 0.15, linetype = "dashed", lineend = "round"),
         panel.grid.minor = element_blank(),
         
-        plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 2, b = 2)),
-        plot.subtitle = element_markdown(size = 13, hjust = 0.5,  color = "grey30", margin = margin(t = 5, b = 10)),
-        plot.caption  = element_markdown(margin = margin(t = 25), size = 8, hjust = 1)
-        
-    )
-
-
-
-
-# plot -----------
-
-
-
-
-p = ggplot(df_plot, aes(x = year, y = avg_close)) +
-    
-    geom_line(
-        linewidth = .75, aes(color = company)
-    ) +
-    
-    geom_point(
-        shape = 21, stroke = .85, size = 4,
-        aes(color = company), fill = "grey95"
-    ) +
-    
-    scale_y_log10() +
-    
-    scale_x_continuous(
-        breaks = c(2010, 2012, 2014, 2016, 2018, 2020, 2022)
-    ) +
-    
-    scale_color_manual(
-        values = col,
-        name = "Companies"
-    ) +
-    
-    labs(
-        title = "Annual Closing Price Trends of Big Tech Giants",
-        subtitle = "Year-over-year growth of Apple, Amazon, Netflix & Tesla from 2010 to 2022",
-        caption = "30DayChartChallenge 2025: <b> Day 23</b> | Source: <b> Big Tech Stock Prices (TidyTuesday) </b> | Graphic: <b>Natasa Anastasiadou</b>",
-        y = "Avg Closing Price (log scale)",
-        fill = "Type"
-    ) +
-
-    theme_minimal(base_family = "Candara") +
-    
-    theme(
-        
-        axis.title.x = element_blank(),
-        
-        axis.title.y = element_text(size = 12),
-        
-        axis.text = element_text(size = 10),
-        
-        legend.position = c(.87, .25),
-        legend.text = element_text(size = 10),         
-        legend.title = element_text(size = 11),        
-        legend.key.size = unit(0.8, "lines"),
-        legend.spacing.y = unit(0.5, "lines"),
-        
-
-        panel.grid.major = element_line(color = "grey75", linetype = "dashed", lineend = "round"),
-        panel.grid.minor = element_blank(),
         
         plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 2, b = 2)),
         plot.subtitle = element_markdown(size = 13, hjust = 0.5,  color = "grey30", margin = margin(t = 5, b = 10)),
         plot.caption  = element_markdown(margin = margin(t = 25), size = 8, hjust = 1),
         
-        
         plot.margin = margin(20, 20, 20, 20),
         
-        plot.background = element_rect(fill = "#e4e4e3", color = NA)
+        plot.background = element_rect(fill = "grey93", color = NA)
         
     )
 
 
 
-p
-
+p 
 
 ggsave(
     plot = p, filename = "Rplot.png",
