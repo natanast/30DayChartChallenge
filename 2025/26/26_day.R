af05c7220f9ac07b9c14c0176baa104ab3a36ffe
NatAnastas <anastasiadounat@gmail.com>
2025-04-26 14:21:57 +0300

    26

--- 2025/26/26_day.R
+++ 2025/26/26_day.R
@@ -11,7 +11,10 @@ library(ggplot2)
 library(stringr)
 library(ggtext)
 library(extrafont)
-library(dplyr)
+library(shadowtext)
+
+library(sf)
+library(usmap)
 
 
 # load data --------
@@ -21,87 +24,176 @@ tornados <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday
     
 # data cleaning -----------
 
-df = tornados[, .(yr, st, slat, slon, elat, elon, fat, loss, inj)]
+df = tornados[, .(yr, st, slat, slon, elat, elon, fat, inj)]
+
+
+# index = which(df$fat > 0 & df$inj > 0 & df$yr > 2000)
+
+# df = df[index]
+
+
+df_inj <- df[, .(total_inj = sum(inj, na.rm = TRUE)), by = st]
+df_fat <- df[ , .(total_fat = sum(fat, na.rm = TRUE)), by = st]
+
+
+
+
+# map --------
+
+map <- us_map() |> 
+    select(state = abbr) |> 
+    st_as_sf()
+
+
+# Merge data with map
+map_inj <- map |> 
+    left_join(df_inj, by = c("state" = "st"))
+
+map_fat <- map |> 
+    left_join(df_fat, by = c("state" = "st"))
+
+
+
+# map_inj = map_inj[!is.na(map_inj$yr), ]
+# map_fat = map_fat[!is.na(map_fat$yr), ]
+
+
+
+
+
+# Calculate centroids for state labels
+centroids <- st_centroid(map_inj)
+centroids_coords <- st_coordinates(centroids)
+map_inj$centroid_x <- centroids_coords[, 1]
+map_inj$centroid_y <- centroids_coords[, 2]
+
+
+
+# Calculate centroids for state labels
+centroids <- st_centroid(map_fat)
+centroids_coords <- st_coordinates(centroids)
+map_fat$centroid_x <- centroids_coords[, 1]
+map_fat$centroid_y <- centroids_coords[, 2]
+
+
+
 
 
 # Plotting -------
 
-col =  c('#2c5769', '#6F99AD', '#ffb39a', '#df7775', '#ab403f')
 
+ggplot(map_inj) +
+    
+    geom_sf(data = map, fill = "grey95", linewidth = 0.05) +
+    
+    geom_sf(aes(fill = total_inj), color = "gray25", linewidth = 0.2)  +
+    
 
-
-p = df_avg_country |>
-    
-    ggplot(aes(x = avg_Exposure, y = avg_Coping, size = avg_WRI, fill = RiskCategory)) +
-    
-    geom_point(
-        shape = 21, 
-        alpha = 0.85, 
-        stroke = 0.2, 
-        color = "white"
-    ) +
-    
-    scale_size(range = c(1.5, 9)) +
-    
-    geom_text(
-        aes(label = label),
-        hjust = 0.5, 
-        vjust = -1.75, 
-        size = 3, 
-        color = "black"
+    geom_shadowtext(
+        aes(x = centroid_x, y = centroid_y, label = state),
+        size = 3.5,
+        family = "Candara",
+        color = "gray10",
+        bg.color = "grey93",
+        bg.r = 0.06
     ) +
 
-    scale_fill_manual(
-        values = rev(col)
+    scale_fill_stepsn(
+        colors = c("#e2e0ff","#a09fcf", "#62628e", "#35375f"),
+        labels = scales::comma,
+        guide = guide_colorsteps(
+            title = "Injuries",
+            barheight = unit(0.5, "lines"),
+            barwidth = unit(10, "lines")
+            
+        )
     ) +
     
     labs(
         title = "Disaster Risk Across Countries: How Exposure and Coping Capacity Shape Vulnerability (2011-2021)",
         subtitle = "Exploring the relationship between countries' exposure to hazards and their coping capacity,<br> with insights into the World Risk Index (WRI).</br>",
-        caption = "30DayChartChallenge 2025: <b> Day 25</b> 
-                   | Source: <b> World Disaster Risk Dataset (Kaggle) </b> 
-                   | Graphic: <b>Natasa Anastasiadou</b>",
-        x = "Average Exposure to Hazards",
-        y = "Average Lack of Coping Capabilities",
-        size = "Average WRI",
-        fill = "Risk Category"
+        caption = "30DayChartChallenge 2025: <b> Day 26</b>
+                       | Source: <b> Tornados (TidyTuesday) </b>
+                       | Graphic: <b>Natasa Anastasiadou</b>",
     ) +
     
-    
-    theme_minimal(base_family = "Candara") +
+    theme_void(base_family = "Candara") +
     
     theme(
-
-        legend.position = "right",
-        legend.title = element_text(size = 10),
-        legend.text = element_text(size = 9),
-        legend.box = "vertical",
         
-        axis.title.y = element_text(size = 11, vjust = 5),
-        axis.title.x = element_text(size = 11, vjust = -2),
+        legend.position = "bottom",
+        legend.title.position = "top",
         
-        axis.text = element_text(size = 10),
+        legend.title = element_text(size = 9, face = "bold", color = "grey30", hjust = .5),
+        legend.text = element_text(size = 7, color = "grey30"),
         
-        plot.title = element_markdown(size = 15, face = "bold", hjust = 0.25, margin = margin(t = 2, b = 2)),
-        plot.subtitle = element_markdown(size = 13, hjust = 0.5,  color = "grey30", margin = margin(t = 5, b = 20)),
-        plot.caption  = element_markdown(margin = margin(t = 35), size = 8, hjust = 1.45),
         
-        panel.grid.major = element_line(color = "grey65", linewidth = 0.25, linetype = "dashed", lineend = "round"),
-        panel.grid.minor = element_blank(),
-        
+        plot.title = element_markdown(size = 15, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
+        plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 5, b = 15)),
+        plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1.03),
         
         plot.margin = margin(20, 20, 20, 20),
         
-        plot.background = element_rect(fill = "grey93", color = NA),
-    ) +
-    
-    guides(
-        size = guide_legend(override.aes = list(shape = 21, color = "grey30", stroke = 0.5)),
-        fill = guide_legend(override.aes = list(size = 5))
+        plot.background = element_rect(fill = "grey93", color = NA)
     )
 
 
-p 
+ggplot(map_fat) +
+
+    geom_sf(data = map, fill = "grey95", linewidth = 0.05) +
+
+    geom_sf(aes(fill = total_fat), color = "gray25", linewidth = 0.2) +
+    
+    geom_shadowtext(
+        aes(x = centroid_x, y = centroid_y, label = state),
+        size = 3.5,
+        family = "Candara",
+        color = "gray10",
+        bg.color = "grey93",
+        bg.r = 0.06
+    ) +
+    
+    scale_fill_stepsn(
+        colors = c("#e2e0ff","#a09fcf", "#62628e", "#35375f"),
+        labels = scales::comma,
+        guide = guide_colorsteps(
+            title = "Fatalities",
+            barheight = unit(0.5, "lines"),
+            barwidth = unit(10, "lines")
+            
+        )
+    ) +
+    
+        labs(
+            title = "Disaster Risk Across Countries: How Exposure and Coping Capacity Shape Vulnerability (2011-2021)",
+            subtitle = "Exploring the relationship between countries' exposure to hazards and their coping capacity,<br> with insights into the World Risk Index (WRI).</br>",
+            caption = "30DayChartChallenge 2025: <b> Day 26</b>
+                       | Source: <b> Tornados (TidyTuesday) </b>
+                       | Graphic: <b>Natasa Anastasiadou</b>",
+        ) +
+    
+    theme_void() +
+    
+    theme(
+        
+        legend.position = "bottom",
+        legend.title.position = "top",
+        
+        legend.title = element_text(size = 9, face = "bold", family = "Candara", color = "grey30", hjust = .5),
+        legend.text = element_text(size = 7, family = "Candara", color = "grey30"),
+        
+        
+        plot.title = element_markdown(size = 20, face = "bold", hjust = 0.5, family = "Candara", margin = margin(t = 25, l = 50)),
+        plot.subtitle = element_markdown(size = 15, hjust = 0.5, family = "Candara", color = "grey30", margin = margin(t = 10, l = 50)),
+        plot.caption = element_markdown(margin = margin(t = 35), size = 8, family = "Candara", hjust = 1.03),
+        
+        plot.margin = margin(20, 20, 20, 20),
+        
+        plot.background = element_rect(fill = "grey93", color = NA)
+    )
+
+
+
 
 ggsave(
     plot = p, filename = "25_day.png",
