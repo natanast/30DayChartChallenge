c05e4908781206797daebd0caf6facedc11d13b6
NatAnastas <anastasiadounat@gmail.com>
2026-03-22 18:09:51 +0200

    d5

---
+++ 2026/05/05_day.R
@@ -0,0 +1,74 @@
+
+
+rm(list = ls())
+gc()
+
+
+# load libraries -------
+
+library(data.table)
+library(ggplot2)
+library(ggtext)
+library(extrafont)
+
+
+
+# load data ------
+
+
+
+# clean data ------
+
+
+
+
+# plot --------
+
+
+# 
+# gr <- ggplot(df_picto, aes(x = x, y = season_label)) +
+#     
+#     
+#     geom_text(label = "🍔", size = 8, family = "Segoe UI Emoji") +
+#     
+#     scale_x_continuous(limits = c(0, 13), breaks = seq(0, 12, by = 2)) +
+#     
+#     labs(
+#         title = "Bob's Burgers: The Short & Long Seasons",
+#         subtitle = "Total unique words spoken per season. Season 2 was cut to just 9 episodes. <br><b>Each 🍔 represents 2,000 words.</b>",
+#         caption = "30DayChartChallenge 2026: <b> Day 2</b>
+#                    | Source: <b> bobsburgers (TidyTuesday | Nov 2024)</b>
+#                    | Graphic: <b>Natasa Anastasiadou</b>",
+#         
+#     ) +
+#     
+#     theme_minimal(base_family = "Candara") +
+#     
+#     theme(
+#         
+#         axis.title = element_blank(),
+#         
+#         axis.text.x = element_text(size = 10, color = "grey30"),
+#         axis.text.y = element_text(size = 12, face = "bold", color = "black", margin = margin(r = 10)),
+#         
+#         panel.grid.major = element_line(linewidth = 0.35, color = "grey85"),
+#         panel.grid.minor = element_blank(),
+#         
+#         plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
+#         plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
+#         plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1),
+#         
+#         plot.background = element_rect(fill = "grey95", color = NA),
+#         plot.margin = margin(20, 20, 20, 20)
+#     )
+# 
+# gr
+
+
+
+# save ---------
+
+ggsave(
+    plot = gr, filename = "Rplot.png",
+    width = 9, height = 9, units = "in", dpi = 600
+)
