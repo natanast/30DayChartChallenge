2de9cd03d3933139de67b45c1846da5a22f203a4
NatAnastas <anastasiadounat@gmail.com>
2026-03-24 15:59:40 +0200

    d6

--- 2026/06/06_day.R
+++ 2026/06/06_day.R
@@ -14,145 +14,6 @@ library(extrafont)
 
 # load data ------
 
-dt <- as.data.table(ToothGrowth)
-
-
-# clean data ------
-
-
-dt[, dose := as.factor(dose)]
-
-
-dt[, supp := fcase(
-    supp == "OJ", "Orange Juice",
-    supp == "VC", "Ascorbic Acid (Supplement)"
-)]
-
-
-col <- c("Orange Juice" = "#b24745", "Ascorbic Acid (Supplement)" = "#5a8192")
-
-
-# plot --------
-
-gr <- ggplot(dt, aes(x = dose, y = len, fill = supp)) +
-    
-    geom_boxplot(
-        width = 0.5, 
-        alpha = 0.75, 
-        outlier.shape = NA, 
-        position = position_dodge(width = 0.6) ,
-        color = "gray30"
-        
-    ) +
-    
-    geom_point(
-        position = position_jitterdodge(jitter.width = 0.1, dodge.width = 0.6),
-        # size = 2,
-        # alpha = 0.8,
-        shape = 21, size = 3.5, stroke = .25, alpha = 1.5,
-        color = "grey20"
-    ) +
-    
-    
-    
-    scale_fill_manual(values = col) +
-    # scale_color_manual(values = col) +
-    
-    labs(
-        title = "Natural vs. Artificial Vitamin C",
-        subtitle = "In a <b>guinea pig model</b>, low doses of Orange Juice promote significantly more cellular<br>growth than artificial supplements. At a high dose (2.0 mg), the advantage vanishes.",
-        caption = "30DayChartChallenge 2026: <b> Day 5 </b> | Source: <b> Crampton (1947) J. Nutr.</b> | Graphic: <b>Natasa Anastasiadou</b>",
-        y = "Odontoblast Length (Cellular Growth)",
-        x = "Vitamin C Dosage (mg/day)"
-    ) +
-    
-    theme_minimal(base_family = "Candara") +
-    
-    theme(
-        
-        legend.position = "top",
-        legend.title = element_blank(),
-        legend.text = element_text(size = 11, face = "bold", color = "grey30"),
-        
-        axis.title.y = element_text(size = 11, face = "bold", color = "grey30", margin = margin(r = 10)),
-        axis.title.x = element_text(size = 11, face = "bold", color = "grey30", margin = margin(t = 10)),
-        
-        axis.text.x = element_text(size = 12, face = "bold", color = "black"),
-        axis.text.y = element_text(size = 12, color = "grey30"),
-        
-        panel.grid.major = element_line(linewidth = 0.35, color = "grey85"),
-        panel.grid.minor = element_blank(),
-        
-        plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-        plot.subtitle = element_markdown(size = 14, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
-        plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1),
-        
-        plot.background = element_rect(fill = "#e4e4e3", color = NA),
-        plot.margin = margin(20, 20, 20, 20)
-    )
-
-gr
-
-# 
-# gr <- ggplot(df_picto, aes(x = x, y = season_label)) +
-#     
-#     
-#     geom_text(label = "🍔", size = 8, family = "Segoe UI Emoji") +
-#     
-#     scale_x_continuous(limits = c(0, 13), breaks = seq(0, 12, by = 2)) +
-#     
-#     labs(
-#         title = "Bob's Burgers: The Short & Long Seasons",
-#         subtitle = "Total unique words spoken per season. Season 2 was cut to just 9 episodes. <br><b>Each 🍔 represents 2,000 words.</b>",
-#         caption = "30DayChartChallenge 2026: <b> Day 2</b>
-#                    | Source: <b> bobsburgers (TidyTuesday | Nov 2024)</b>
-#                    | Graphic: <b>Natasa Anastasiadou</b>",
-#         
-#     ) +
-#     
-#     theme_minimal(base_family = "Candara") +
-#     
-#     theme(
-#         
-#         axis.title = element_blank(),
-#         
-#         axis.text.x = element_text(size = 10, color = "grey30"),
-#         axis.text.y = element_text(size = 12, face = "bold", color = "black", margin = margin(r = 10)),
-#         
-#         panel.grid.major = element_line(linewidth = 0.35, color = "grey85"),
-#         panel.grid.minor = element_blank(),
-#         
-# plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-# plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
-# plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1),
-#         
-#         plot.background = element_rect(fill = "grey95", color = NA),
-#         plot.margin = margin(20, 20, 20, 20)
-#     )
-# 
-# gr
-
-
-
-# save ---------
-
-ggsave(
-    plot = gr, filename = "Rplot.png",
-    width = 9, height = 9, units = "in", dpi = 600
-)
-
-
-rm(list = ls())
-gc()
-
-# load libraries -------
-library(data.table)
-library(ggplot2)
-library(ggtext)
-library(extrafont)
-
-# 1. Load data ------
-# Extracted from official RSF matrix (2020 vs 2025)
 dt <- data.table(
     country = c("Czech Republic", "Montenegro", "Moldova", "Lithuania", "Poland", 
                 "Russia", "Belarus", "Georgia", "Cyprus", "Kosovo"),
@@ -162,7 +23,8 @@ dt <- data.table(
                    24.57, 25.73, 50.53, 59.04, 52.73)
 )
 
-# 2. Clean data ------
+# clean data ------
+
 dt[, change := score_2025 - score_2020]
 dt[, status := fifelse(change > 0, "Improved", "Declined")]
 
@@ -171,14 +33,16 @@ dt[, country := factor(country, levels = dt[order(change)]$country)]
 
 col <- c("Declined" = "#b24745", "Improved" = "#5a8192")
 
-# 3. Plot --------
+
+# plot -----
+
 gr <- ggplot(dt) +
     
     geom_segment(aes(x = score_2020, xend = score_2025, y = country, yend = country), color = "grey75", linewidth = 2) +
     
     geom_point(aes(x = score_2020, y = country), color = "grey60", size = 3) +
     
-    geom_point(aes(x = score_2025, y = country, fill = status), shape = 21, size = 4, stroke = 0.5, color = "grey20") +
+    geom_point(aes(x = score_2025, y = country, fill = status), shape = 21, size = 4, stroke = 0.5) +
     
     # Text labels showing the precise 2025 score (rounded to 1 decimal for cleanliness)
     geom_text(
@@ -219,11 +83,73 @@ gr <- ggplot(dt) +
 
 gr
 
-# 4. Save ---------
+
+# plot --------
+# 
+# gr <- ggplot(dt, aes(x = dose, y = len, fill = supp)) +
+#     
+#     geom_boxplot(
+#         width = 0.5, 
+#         alpha = 0.75, 
+#         outlier.shape = NA, 
+#         position = position_dodge(width = 0.6) ,
+#         color = "gray30"
+#         
+#     ) +
+#     
+#     geom_point(
+#         position = position_jitterdodge(jitter.width = 0.1, dodge.width = 0.6),
+#         # size = 2,
+#         # alpha = 0.8,
+#         shape = 21, size = 3.5, stroke = .25, alpha = 1.5,
+#         color = "grey20"
+#     ) +
+#     
+#     
+#     
+#     scale_fill_manual(values = col) +
+#     # scale_color_manual(values = col) +
+#     
+#     labs(
+#         title = "Natural vs. Artificial Vitamin C",
+#         subtitle = "In a <b>guinea pig model</b>, low doses of Orange Juice promote significantly more cellular<br>growth than artificial supplements. At a high dose (2.0 mg), the advantage vanishes.",
+#         caption = "30DayChartChallenge 2026: <b> Day 5 </b> | Source: <b> Crampton (1947) J. Nutr.</b> | Graphic: <b>Natasa Anastasiadou</b>",
+#         y = "Odontoblast Length (Cellular Growth)",
+#         x = "Vitamin C Dosage (mg/day)"
+#     ) +
+#     
+#     theme_minimal(base_family = "Candara") +
+#     
+#     theme(
+#         
+#         legend.position = "top",
+#         legend.title = element_blank(),
+#         legend.text = element_text(size = 11, face = "bold", color = "grey30"),
+#         
+#         axis.title.y = element_text(size = 11, face = "bold", color = "grey30", margin = margin(r = 10)),
+#         axis.title.x = element_text(size = 11, face = "bold", color = "grey30", margin = margin(t = 10)),
+#         
+#         axis.text.x = element_text(size = 12, face = "bold", color = "black"),
+#         axis.text.y = element_text(size = 12, color = "grey30"),
+#         
+#         panel.grid.major = element_line(linewidth = 0.35, color = "grey85"),
+#         panel.grid.minor = element_blank(),
+#         
+#         plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
+#         plot.subtitle = element_markdown(size = 14, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
+#         plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1),
+#         
+#         plot.background = element_rect(fill = "#e4e4e3", color = NA),
+#         plot.margin = margin(20, 20, 20, 20)
+#     )
+
+
+
+# save ---------
+
 ggsave(
-    "Day6_DataDay_Top5_Exact.png", 
-    plot = gr, 
-    width = 9, 
-    height = 7, 
-    dpi = 600
+    plot = gr, filename = "Rplot.png",
+    width = 9, height = 9, units = "in", dpi = 600
 )
+
+
