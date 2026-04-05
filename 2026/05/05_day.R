e91d33d7934280944eb3fe6c3dde354bf1e9edb3
NatAnastas <anastasiadounat@gmail.com>
2026-04-05 16:30:09 +0300

    d4

--- 2026/05/05_day.R
+++ 2026/05/05_day.R
@@ -19,9 +19,7 @@ dt <- as.data.table(ToothGrowth)
 
 # clean data ------
 
-
 dt[, dose := as.factor(dose)]
-
 
 dt[, supp := fcase(
     supp == "OJ", "Orange Juice",
@@ -105,33 +103,6 @@ gr
 #         title = "Bob's Burgers: The Short & Long Seasons",
 #         subtitle = "Total unique words spoken per season. Season 2 was cut to just 9 episodes. <br><b>Each 🍔 represents 2,000 words.</b>",
 #         caption = "30DayChartChallenge 2026: <b> Day 2</b>
-#                    | Source: <b> bobsburgers (TidyTuesday | Nov 2024)</b>
-#                    | Graphic: <b>Natasa Anastasiadou</b>",
-#         
-#     ) +
-#     
-#     theme_minimal(base_family = "Candara") +
-#     
-#     theme(
-#         
-#         axis.title = element_blank(),
-#         
-#         axis.text.x = element_text(size = 10, color = "grey30"),
-#         axis.text.y = element_text(size = 12, face = "bold", color = "black", margin = margin(r = 10)),
-#         
-#         panel.grid.major = element_line(linewidth = 0.35, color = "grey85"),
-#         panel.grid.minor = element_blank(),
-#         
-# plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-# plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
-# plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1),
-#         
-#         plot.background = element_rect(fill = "grey95", color = NA),
-#         plot.margin = margin(20, 20, 20, 20)
-#     )
-# 
-# gr
-
 
 
 # save ---------
