eac4218ed674ebdb2bd4b66a4846dc155ff879b6
NatAnastas <anastasiadounat@gmail.com>
2026-04-10 16:18:31 +0300

    d10

--- 2026/10/10_day.R
+++ 2026/10/10_day.R
@@ -90,42 +90,6 @@ gr <- ggplot(dt_clean, aes(x = Decade, y = spotify_popularity, fill = Decade)) +
 gr
 
 
-
-#     
-#     labs(
-#         title = "Penguin Demographics Across the Palmer Archipelago",
-#         subtitle = "<b>Each dot</b> represents <b>1%</b> of the total penguin population on that island.",
-#         caption = "30DayChartChallenge 2026: <b> Day 1</b>
-#                    | Source: <b> palmerpenguins (R package)</b>
-#                    | Graphic: <b>Natasa Anastasiadou</b>",
-#         
-#     ) +
-#     
-#     theme_minimal(base_family = "Candara") +
-#     
-#     theme(
-#         legend.position = "bottom",
-#         
-#         axis.title = element_blank(),
-#         # axis.text = element_blank(),
-#         
-#         axis.text.x = element_blank(),
-#         axis.text.y = element_text(size = 9.5),
-#         
-#         strip.text = element_text(size = 9.5),
-#         
-#         panel.grid.major = element_line(linewidth = 0.35, color = "grey85"),
-#         panel.grid.minor = element_blank(),
-#         
-        # plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-        # plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
-        # plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1),
-
-#         plot.background = element_rect(fill = "grey95", color = NA),
-#         plot.margin = margin(20, 20, 20, 20)
-#     )
-
-
 # save ---------
 
 ggsave(
