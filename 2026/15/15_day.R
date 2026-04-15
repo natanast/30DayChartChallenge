d9bcf6516377602528cfd2fa3687ecace7e5183c
NatAnastas <anastasiadounat@gmail.com>
2026-04-15 14:32:53 +0300

    d15

--- 2026/15/15_day.R
+++ 2026/15/15_day.R
@@ -56,7 +56,7 @@ gr <- ggplot(dt_plot, aes(x = lon, y = duration_min)) +
     geom_point(
         aes(fill = state),
         alpha = 0.8, 
-        size = 2, 
+        size = 2.5, 
         stroke = 0.15, 
         color = "white",
         shape = 21
@@ -70,14 +70,16 @@ gr <- ggplot(dt_plot, aes(x = lon, y = duration_min)) +
         lineend = "round"
     ) +
     
-   scale_fill_manual(values = cols, name = "State") +
+    scale_fill_manual(values = cols, name = "State") +
     
-    guides(fill = guide_legend(nrow = 1)) +
+    # guides(fill = guide_legend(nrow = 1)) +
+    
+    guides(fill = guide_legend(nrow = 1, override.aes = list(size = 3))) +
     
     labs(
-        title = "Fading to black: The 2024 eclipse",
-        subtitle = "Visualizing the duration of totality across the United States.<br>As the moon's shadow swept from Texas to Maine, the window of total darkness steadily decreased.",
-        caption = "30DayChartChallenge 2026: <b> Day 15 (Correlation) </b> | Source: <b> NASA </b> | Graphic: <b>Natasa Anastasiadou</b>",
+        title = "Chasing the Shadow: The 2024 Solar Eclipse",
+        subtitle = "As the moon's shadow swept from Texas to Maine, the window of total darkness steadily decreased. <br>Each point represents a city or town in the path of totality.",
+        caption = "30DayChartChallenge 2026: <b> Day 15 </b> | Source: <b> NASA (TidyTuesday) </b> | Graphic: <b>Natasa Anastasiadou</b>",
         x = "Longitude (Degrees West)",
         y = "Duration of Totality (Minutes)"
     ) +
@@ -91,18 +93,20 @@ gr <- ggplot(dt_plot, aes(x = lon, y = duration_min)) +
         plot.background = element_rect(fill = "#f2f2f2", color = NA),
         panel.background = element_rect(fill = "#f2f2f2", color = NA),
         
-        axis.text = element_text(size = 9, color = "grey40", face = "bold"),
-        axis.title.x = element_text(size = 11, face = "bold", color = "grey30", margin = margin(t = 15)),
-        axis.title.y = element_text(size = 11, face = "bold", color = "grey30", margin = margin(r = 15)),
+        axis.text = element_text(size = 10, color = "grey40", face = "bold"),
+        axis.title.x = element_text(size = 12, face = "bold", color = "grey30", margin = margin(t = 15)),
+        axis.title.y = element_text(size = 12, face = "bold", color = "grey30", margin = margin(r = 15)),
         
         legend.position = "bottom",
-        legend.title = element_text(face = "bold", size = 10),
+        legend.title = element_text(face = "bold", size = 11),
+        legend.text = element_text(size = 10),
+        # legend.key.size = unit(0.8, "cm"),
         
-        plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-        plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
-        plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1),
+        plot.title = element_markdown(size = 18, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
+        plot.subtitle = element_markdown(size = 15, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
+        plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1),
         
-        plot.margin = margin(30, 30, 30, 30)
+        plot.margin = margin(20, 20, 20, 20)
     )
 
 gr
