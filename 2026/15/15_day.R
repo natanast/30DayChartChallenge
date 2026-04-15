75f8290cb39aa68e6712780f039d73a837fbef28
NatAnastas <anastasiadounat@gmail.com>
2026-04-15 14:20:02 +0300

    d15

--- 2026/15/15_day.R
+++ 2026/15/15_day.R
@@ -21,17 +21,25 @@ dt <- "https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2
 
 # clean data -----
 
-# 2. Calculate duration and filter ------
 dt[, duration_min := as.numeric(as.ITime(eclipse_4) - as.ITime(eclipse_3)) / 60]
 
-# Bring back all 6 states
+
 target_states <- c("TX", "AR", "IN", "OH", "NY", "ME")
 dt_plot <- dt[state %in% target_states & duration_min > 0]
 
-# Convert state to a factor so the legend stays in geographic order (South to North)
+
 dt_plot[, state := factor(state, levels = c("TX", "AR", "IN", "OH", "NY", "ME"))]
 
-# 3. Create the Palette -------
+
+dt_extremes <- rbind(
+    dt_plot[order(-duration_min)][1], 
+    dt_plot[order(duration_min)][1]   
+)
+
+
+
+# plot --------
+
 cols <- c(
     "TX" = "#b25c56",  
     "AR" = "#e8998f",  
@@ -42,11 +50,6 @@ cols <- c(
 )
 
 
-dt_extremes <- rbind(
-    dt_plot[order(-duration_min)][1], 
-    dt_plot[order(duration_min)][1]   
-)
-# plot --------
 
 gr <- ggplot(dt_plot, aes(x = lon, y = duration_min)) +
     
@@ -95,54 +98,14 @@ gr <- ggplot(dt_plot, aes(x = lon, y = duration_min)) +
         legend.position = "bottom",
         legend.title = element_text(face = "bold", size = 10),
         
-        plot.title = element_text(size = 20, face = "bold", color = "black", hjust = 0.5),
-        plot.subtitle = element_markdown(size = 11, color = "grey40", hjust = 0.5, lineheight = 1.3, margin = margin(b = 20)),
-        plot.caption = element_markdown(margin = margin(t = 20), size = 8, color = "grey50", hjust = 1),
+        plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
+        plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
+        plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1),
         
         plot.margin = margin(30, 30, 30, 30)
     )
 
 gr
-
-
-# gr <- ggplot(df_picto, aes(x = x, y = season_label)) +
-#     
-#     
-#     geom_text(label = "🍔", size = 8, family = "Segoe UI Emoji") +
-#     
-#     scale_x_continuous(limits = c(0, 13), breaks = seq(0, 12, by = 2)) +
-#     
-#     labs(
-#         title = "Bob's Burgers: The Short & Long Seasons",
-#         subtitle = "Total unique words spoken per season. Season 2 was cut to just 9 episodes. <br><b>Each 🍔 represents 2,000 words.</b>",
-#         caption = "30DayChartChallenge 2026: <b> Day 2</b>
-#                    | Source: <b> bobsburgers (TidyTuesday | Nov 2024)</b>
-#                    | Graphic: <b>Natasa Anastasiadou</b>",
-#         
-#     ) +
-#     
-#     theme_minimal(base_family = "Candara") +
-#     
-#     theme(
-#         
-#         axis.title = element_blank(),
-#         
-#         axis.text.x = element_text(size = 10, color = "grey30"),
-#         axis.text.y = element_text(size = 12, face = "bold", color = "black", margin = margin(r = 10)),
-#         
-#         panel.grid.major = element_line(linewidth = 0.35, color = "grey85"),
-#         panel.grid.minor = element_blank(),
-#         
-        # plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-        # plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
-        # plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1),
-#         
-#         plot.background = element_rect(fill = "grey95", color = NA),
-#         plot.margin = margin(20, 20, 20, 20)
-#     )
-# 
-# gr
-# 
 
 
 # save ---------
