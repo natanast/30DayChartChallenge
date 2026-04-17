a85f22f8d7943fed461bfdd88b9ee697cd458dc8
NatAnastas <anastasiadounat@gmail.com>
2026-04-17 09:13:11 +0300

    d17

--- 2026/17/17_day.R
+++ 2026/17/17_day.R
@@ -11,7 +11,8 @@ library(ggplot2)
 library(ggtext)
 library(extrafont)
 library(ggrepel)
-
+library(extrafont)
+library(ggrepel)
 
 # load data ------
 
@@ -108,8 +109,9 @@ gr <- ggplot(dt_plot, aes(x = pct, y = Class, fill = category)) +
         axis.text.x = element_text(family = "mono", size = 9),
         axis.line.x = element_line(color = "black"),
         
-        plot.title = element_text(face = "bold", size = 14, hjust = 0.5, family = "serif"),
-        plot.subtitle = element_text(size = 11, hjust = 0.5, family = "mono", margin = margin(b=30)),
+        plot.title = element_markdown(size = 18, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
+        plot.subtitle = element_markdown(size = 15, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
+        plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1),
         
         plot.margin = margin(20, 20, 20, 20)
     ) 
@@ -117,50 +119,11 @@ gr <- ggplot(dt_plot, aes(x = pct, y = Class, fill = category)) +
 gr
 
 
-
-# gr <- ggplot(df_picto, aes(x = x, y = season_label)) +
-#     
-#     
-#     geom_text(label = "🍔", size = 8, family = "Segoe UI Emoji") +
-#     
-#     scale_x_continuous(limits = c(0, 13), breaks = seq(0, 12, by = 2)) +
-#     
-    # labs(
-    #     title = "Bob's Burgers: The Short & Long Seasons",
-    #     subtitle = "Total unique words spoken per season. Season 2 was cut to just 9 episodes. <br><b>Each 🍔 represents 2,000 words.</b>",
-    #     caption = "30DayChartChallenge 2026: <b> Day 2</b>
-    #                | Source: <b> bobsburgers (TidyTuesday | Nov 2024)</b>
-    #                | Graphic: <b>Natasa Anastasiadou</b>",
-    # 
-    # ) +
-#     
-#     theme_minimal(base_family = "Candara") +
-#     
-#     theme(
-#         
-#         axis.title = element_blank(),
-#         
-#         axis.text.x = element_text(size = 10, color = "grey30"),
-#         axis.text.y = element_text(size = 12, face = "bold", color = "black", margin = margin(r = 10)),
-#         
-#         panel.grid.major = element_line(linewidth = 0.35, color = "grey85"),
-#         panel.grid.minor = element_blank(),
-#         
-        # plot.title = element_markdown(size = 16, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-        # plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
-        # plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1),
-#         
-#         plot.background = element_rect(fill = "grey95", color = NA),
-#         plot.margin = margin(20, 20, 20, 20)
-#     )
-
-
-
 # save ---------
 
 ggsave(
     plot = gr, filename = "Rplot.png",
-    width = 10, height = 10, units = "in", dpi = 600
+    width = 9, height = 8, units = "in", dpi = 600
 )
 
 
