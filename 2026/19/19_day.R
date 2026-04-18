2b7cc4d8cac5380602a8527dc912b44d5f2fa8a0
NatAnastas <anastasiadounat@gmail.com>
2026-04-18 16:05:51 +0300

    d19

--- 2026/19/19_day.R
+++ 2026/19/19_day.R
@@ -79,9 +79,9 @@ gr <- ggplot(df, aes(x = as.numeric(year), y = avg_price, color = company_name))
             label = paste0("$", round(avg_price)),
             color = company_name
         ),
-        size = 4,
-        vjust = -1.2, 
-        hjust = 0.5,
+        size = 3.75,
+        vjust = 2.5, 
+        hjust = 0.3,
         family = "Candara",
         fontface = "bold"
     ) +
@@ -89,6 +89,7 @@ gr <- ggplot(df, aes(x = as.numeric(year), y = avg_price, color = company_name))
     facet_wrap('~company_name', ncol = 2) +
     
     scale_x_continuous(breaks = c(2010, 2015, 2020, 2024)) +
+    
     scale_color_manual(values = col) +
     scale_fill_manual(values = col) +
     
@@ -105,23 +106,23 @@ gr <- ggplot(df, aes(x = as.numeric(year), y = avg_price, color = company_name))
     theme(
         legend.position = "none",
         
-        plot.title = element_markdown(size = 16, face = "bold", color = "grey20", hjust = 0.5, margin = margin(t = 2, b = 5)),
-        plot.subtitle = element_markdown(size = 11, hjust = 0.5, color = "grey40", margin = margin(t = 5, b = 20)),
-        plot.caption = element_markdown(margin = margin(t = 10), size = 7.5, hjust = 1),
+        plot.title = element_markdown(size = 18, face = "bold", color = "grey20", hjust = 0.5, margin = margin(t = 2, b = 5)),
+        plot.subtitle = element_markdown(size = 15, hjust = 0.5, color = "grey40", margin = margin(t = 5, b = 20)),
+        plot.caption = element_markdown(margin = margin(t = 10), size = 10, hjust = 1),
         
-        panel.grid.major = element_line(linewidth = .25, color = "grey80", linetype = "dashed", lineend = "round"),
-        panel.grid.minor = element_line(linewidth = .25, color = "grey80", linetype = "dashed", lineend = "round"),
+        panel.grid.major = element_line(linewidth = .25, color = "grey85", linetype = "dashed", lineend = "round"),
+        panel.grid.minor = element_line(linewidth = .25, color = "grey85", linetype = "dashed", lineend = "round"),
         
-        axis.title.y = element_text(size = 10, vjust = 5, face = "bold", color = "grey30"),
+        axis.title.y = element_text(size = 12, vjust = 5, face = "bold", color = "grey30"),
         
-        axis.text.x = element_text(size = 8, angle = 45, hjust = 1, color = "grey40"), 
-        axis.text.y = element_text(size = 8, color = "grey40"),
+        axis.text.x = element_text(size = 10, angle = 45, hjust = 1, color = "grey40"), 
+        axis.text.y = element_text(size = 10, color = "grey40"),
         
-        strip.text.x.top = element_text(size = 11, face = "bold", margin = margin(b = 10), color = "grey10"),
+        strip.text.x.top = element_text(size = 12, face = "bold", margin = margin(b = 10), color = "grey10"),
         
-        plot.background = element_rect(fill = "#e4e4e3", color = NA),
+        plot.background = element_rect(fill = "grey95", color = NA),
         
-        plot.margin = margin(20, 25, 20, 20)
+        plot.margin = margin(20, 20, 20, 20)
     ) 
 
 gr
@@ -131,6 +132,6 @@ gr
 
 ggsave(
    plot = gr, filename = "Rplot.png",
-   width = 9, height = 9, units = "in", dpi = 600
+   width = 10, height = 10, units = "in", dpi = 600
 )
 
