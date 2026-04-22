48654ba05fb6400e52af713318f936c332ff7424
NatAnastas <anastasiadounat@gmail.com>
2026-04-22 12:52:14 +0300

    d22

--- 2026/22/22_day.R
+++ 2026/22/22_day.R
@@ -45,12 +45,13 @@ dt_ai$tool <- dt_ai$tool |> factor(levels = custom_order)
 # plot --------
 
 cols <- c(
-    "#628b9c",  # ChatGPT
-    "#c28d75",  # Copilot
-    "#8aa39b",  # Gemini
-    "#b25c56",  # Claude
-    "#7AA6DC"   # DeepSeek
+    "#b25c56",  
+    "#e8998f",  
+    "#7AA6DC",  
+    "#6785ae",  
+    "#4A6990"   
 )
+
 
 names(cols) <- levels(dt_ai$tool)
 
@@ -66,7 +67,7 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
         y = 80,                    
         label = "ChatGPT",
         hjust = 0,
-        size = 3,                
+        size = 3.5,                
         fontface = "bold",
         color = cols["ChatGPT"]    
     ) +
@@ -75,10 +76,10 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
     annotate(
         "text", 
         x = as.Date("2026-04-05"), 
-        y = 46,                    
+        y = 45,                    
         label = "Copilot",
         hjust = 0,
-        size = 3,                
+        size = 3.5,                
         fontface = "bold",
         color = cols["Copilot"]    
     ) +
@@ -90,7 +91,7 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
         y = 38,                    
         label = "Claude",
         hjust = 0,
-        size = 3,                
+        size = 3.5,                
         fontface = "bold",
         color = cols["Claude"]    
     ) +
@@ -99,10 +100,10 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
     annotate(
         "text", 
         x = as.Date("2026-04-05"), 
-        y = 32.5,                    
+        y = 32,                    
         label = "DeepSeek",
         hjust = 0,
-        size = 3,                
+        size = 3.5,                
         fontface = "bold",
         color = cols["DeepSeek"]    
     ) +
@@ -114,7 +115,7 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
         y = 15,                    
         label = "Gemini",
         hjust = 0,
-        size = 3,                
+        size = 3.5,                
         fontface = "bold",
         color = cols["Gemini"]    
     ) +
@@ -129,7 +130,7 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
         expand = expansion(mult = c(0, 0.15)) 
     ) +
     
-    scale_fill_manual(values = cols, name = "AI Tool") +
+    scale_fill_manual(values = cols) +
     
     guides(fill = guide_legend(
         title.position = "top", 
@@ -139,8 +140,8 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
     )) +
     
     labs(
-        title = "The AI Gold Rush: Search Interest Over Time",
-        subtitle = "Cumulative global search volume for the leading Large Language Models.<br>The stacked area reveals the total growth of the category alongside individual market share.",
+        title = "The Rise of the \"New Tools\"",
+        subtitle = "Cumulative Google search volume for the leading Large Language Models. <br> Values are indexed from 0 to 100, where 100 is the absolute maximum search volume in this timeframe.",
         caption = "30DayChartChallenge 2026: <b> Day 22 </b> | Source: <b> Google Trends </b> | Graphic: <b>Natasa Anastasiadou</b>",
         x = "Year",
         y = "Cumulative Search Interest (Index)"
@@ -152,18 +153,18 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
         panel.grid.major = element_line(linewidth = .15, color = "grey80", linetype = "dashed"),
         panel.grid.minor = element_blank(),
         
-        axis.text.x = element_text(size = 10, color = "grey40", face = "bold", margin = margin(t = 10)),
-        axis.text.y = element_text(size = 10, color = "grey40", face = "bold", margin = margin(r = 10)), 
+        axis.text.x = element_text(size = 11, color = "grey40", face = "bold", margin = margin(t = 10)),
+        axis.text.y = element_text(size = 11, color = "grey40", face = "bold", margin = margin(r = 10)), 
         
-        axis.title.x = element_text(size = 12, face = "bold", color = "grey30", margin = margin(t = 15)),
-        axis.title.y = element_text(size = 12, face = "bold", color = "grey30", margin = margin(r = 15)),
+        axis.title.x = element_text(size = 13, face = "bold", color = "grey30", margin = margin(t = 15)),
+        axis.title.y = element_text(size = 13, face = "bold", color = "grey30", margin = margin(r = 15)),
         
-        legend.position = "bottom",
-        legend.title = element_text(face = "bold", size = 10),
-        legend.text = element_text(size = 9),
+        legend.position = "none",
+        # legend.title = element_text(face = "bold", size = 10),
+        # legend.text = element_text(size = 9),
         
         plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-        plot.subtitle = element_markdown(size = 14, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
+        plot.subtitle = element_markdown(size = 13, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
         plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1),
         
         plot.background = element_rect(fill = "grey95", color = NA),
