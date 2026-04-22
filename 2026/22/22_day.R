91caddab77e3c2e10dd3ab3e0ab9521d2c189245
NatAnastas <anastasiadounat@gmail.com>
2026-04-22 12:17:12 +0300

    d22

--- 2026/22/22_day.R
+++ 2026/22/22_day.R
@@ -45,11 +45,11 @@ dt_ai$tool <- dt_ai$tool |> factor(levels = custom_order)
 # plot --------
 
 cols <- c(
-    "#10a37f",  # ChatGPT
-    "#ffb900",  # Copilot
-    "#1a73e8",  # Gemini
-    "#d97757",  # Claude
-    "#4d6bfe"   # DeepSeek
+    "#628b9c",  # ChatGPT
+    "#c28d75",  # Copilot
+    "#8aa39b",  # Gemini
+    "#b25c56",  # Claude
+    "#7AA6DC"   # DeepSeek
 )
 
 names(cols) <- levels(dt_ai$tool)
@@ -123,6 +123,12 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
         expand = c(0, 0)
     ) +
     
+    scale_x_date(
+        date_breaks = "1 year", 
+        date_labels = "%Y",
+        expand = expansion(mult = c(0, 0.15)) 
+    ) +
+    
     scale_fill_manual(values = cols, name = "AI Tool") +
     
     guides(fill = guide_legend(
@@ -143,13 +149,12 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
     theme_minimal(base_family = "Candara") +
     
     theme(
-        # Horizontal lines are better for area charts to read the height
-        panel.grid.major.x = element_blank(),
-        panel.grid.major.y = element_line(color = "grey70", linetype = "dashed", linewidth = .35),
+        panel.grid.major = element_line(linewidth = .15, color = "grey80", linetype = "dashed"),
         panel.grid.minor = element_blank(),
         
         axis.text.x = element_text(size = 10, color = "grey40", face = "bold", margin = margin(t = 10)),
         axis.text.y = element_text(size = 10, color = "grey40", face = "bold", margin = margin(r = 10)), 
+        
         axis.title.x = element_text(size = 12, face = "bold", color = "grey30", margin = margin(t = 15)),
         axis.title.y = element_text(size = 12, face = "bold", color = "grey30", margin = margin(r = 15)),
         
@@ -161,81 +166,12 @@ gr <- ggplot(dt_ai, aes(x = Time, y = interest, fill = tool)) +
         plot.subtitle = element_markdown(size = 14, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
         plot.caption = element_markdown(margin = margin(t = 35), size = 9, hjust = 1),
         
-        plot.background = element_rect(fill = "grey93", color = NA),
+        plot.background = element_rect(fill = "grey95", color = NA),
         plot.margin = margin(20, 20, 20, 20)
     )
 
 gr
 
-
-
-
-
-# plot -------
-# 
-# cols <- c("Above 0" = "#b24745", "Below 0" = "#00429d")
-# 
-# 
-# gr <- ggplot(dt_plot, aes(x = Year, y = Anomaly, color = Temp_Type, fill = Temp_Type)) +
-#     
-#     geom_hline(yintercept = 0, color = "grey40", linewidth = 0.7) +
-#     
-#     geom_segment(
-#         aes(x = Year, xend = Year, y = 0, yend = Anomaly), 
-#         linewidth = 0.65, 
-#         alpha = 0.85
-#     ) +
-#     
-#     geom_point(
-#         size = 3, 
-#         shape = 21,
-#         stroke = 0.25
-#     ) +
-#     
-#     scale_color_manual(
-#         values = cols |> darken(.15), 
-#         guide = "none"
-#     ) +
-#     
-#     scale_fill_manual(
-#         values = cols |> lighten(.15), 
-#         guide = "none"
-#     ) +
-#     
-#     scale_x_continuous(breaks = seq(1880, 2020, by = 20)) +
-#     
-#     labs(
-#         title = "The Escalation of Global Temperatures",
-#         subtitle = "Annual global temperature anomalies (°C) compared to the 1951-1980 average",
-#         caption = "30DayChartChallenge 2024: <b>Day 20</b> | Source: <b>NASA GISS (Kaggle)</b> | Graphic: <b>Natasa Anastasiadou</b>",
-#         x = "",
-#         y = "Temperature Anomaly (°C)"
-#     ) +
-#     
-#     theme_minimal(base_family = "Candara") +
-#     
-#     theme(
-#        
-#         legend.position = "none",
-#         
-#         plot.title = element_markdown(size = 18, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-#         plot.subtitle = element_markdown(size = 16, hjust = 0.5, color = "grey30", margin = margin(t = 5, b = 15)),
-#         plot.caption = element_markdown(margin = margin(t = 35), size = 10, hjust = 1),
-#         
-#         panel.grid.major.y = element_line(linewidth = .25, color = "grey70", linetype = "dashed"),
-#         panel.grid.minor.y = element_blank(),
-#         
-#         panel.grid.major.x = element_line(linewidth = .25, color = "grey70", linetype = "dashed"), 
-#         panel.grid.minor.x = element_blank(),
-#         
-#         axis.title.y = element_text(size = 13, vjust = 5, face = "bold", color = "grey30"),
-#         axis.text = element_text(size = 11, color = "grey40"),
-#         
-#         plot.background = element_rect(fill = "grey95", color = NA),
-#         plot.margin = margin(20, 20, 20, 20)
-#     )
-# 
-# gr
 
 # save ---------
 
