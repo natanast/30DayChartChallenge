0e15e07f5b0d8dd3810c3f8886363042d4fb9036
NatAnastas <anastasiadounat@gmail.com>
2026-04-23 09:35:29 +0300

    d23

--- 2026/23/23_day.R
+++ 2026/23/23_day.R
@@ -41,20 +41,34 @@ dt_tv[, season_label := str_c("Season ", season, " (", start_year, ")")]
 label_order <- unique(dt_tv[order(as.numeric(season)), season_label])
 dt_tv$season_label <- factor(dt_tv$season_label, levels = label_order)
 
+dt_labels <- dt_tv[, .(
+    avg_val = unique(round(season_avg, 1)), 
+    max_ep = max(ep_num)
+), by = season_label]
+
 
 # plot -------
 
 cols <- c("TRUE" = "#b25c56", "FALSE" = "#4A6990")
 
+
 gr <- ggplot(dt_tv, aes(x = ep_num, y = y_pos, fill = is_win)) +
     
-    # Baseline for the sparkline
     geom_hline(yintercept = 0, color = "grey80", linewidth = 0.5) +
     
-    # width = 0.8 leaves just a tiny, clean gap between the episodes
     geom_col(width = 0.65, show.legend = FALSE) +
     
-    # Facet by our new custom label, placing it on the left
+    geom_text(
+        data = dt_labels,
+        aes(x = 25.4, y = 0.5, label = str_c("Avg: ", avg_val)), 
+        inherit.aes = FALSE,
+        family = "Candara",
+        size = 3,
+        color = "grey30",
+        fontface = "bold.italic",
+        hjust = 0
+    ) +
+    
     facet_grid(season_label ~ ., switch = "y") +
     
     scale_fill_manual(values = cols) +
@@ -69,7 +83,7 @@ gr <- ggplot(dt_tv, aes(x = ep_num, y = y_pos, fill = is_win)) +
     theme_void(base_family = "Candara") +
     
     theme(
-        # Format the Season (Year) labels on the left
+        
         strip.text.y.left = element_text(size = 11, face = "bold", color = "grey30", angle = 0, hjust = 1, margin = margin(r = 15)),
         
         plot.title = element_markdown(size = 18, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
@@ -77,84 +91,15 @@ gr <- ggplot(dt_tv, aes(x = ep_num, y = y_pos, fill = is_win)) +
         plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1, lineheight = 1.2),
         
         plot.background = element_rect(fill = "#e4e4e3", color = NA),
-        plot.margin = margin(20, 20, 20, 20)
+        plot.margin = margin(20, 30, 20, 20)
     )
 
 gr
-
-# plot --------
-# 
-# 
-# gr <- ggplot(dt_long, aes(x = Year, y = Spending, group = Gift)) +
-#     
-#     geom_line(aes(color = Gift), linewidth = 1) +
-#     
-# 
-#     geom_point(
-#         aes(color = Gift),
-#         shape = 21, 
-#         stroke = 1, 
-#         size = 4.5,
-#         fill = "grey95"
-#     ) +
-#     
-#     geom_text_repel(
-#         data = dt_long[Year == "2010"],
-#         aes(label = paste0(Gift, " ($", round(Spending), ")"), color = Gift),
-#         hjust = 1.1, 
-#         direction = "y", 
-#         size = 4.5, 
-#         fontface = "bold", 
-#         segment.color = NA
-#     ) +
-#     
-#     
-#     geom_text_repel(
-#         data = dt_long[Year == "2022"],
-#         aes(label = paste0("($", round(Spending), ") ", Gift), color = Gift),
-#         hjust = -0.1, 
-#         direction = "y", 
-#         size = 4.5, 
-#         fontface = "bold", 
-#         segment.color = NA
-#     ) +
-#     
-#     scale_color_manual(values = col) +
-#     
-#     
-#     scale_x_discrete(expand = expansion(mult = 0.5)) +
-#     
-#     labs(
-#         title = "The Rising Cost of Romance",
-#         subtitle = "Average per-person Valentine's Day spending in the US: 2010 vs 2022.",
-#         caption = "30DayChartChallenge 2026: <b> Day 4 (Slope)</b> | Source: <b> NRF (TidyTuesday)</b> | Graphic: <b>Natasa Anastasiadou</b>"
-#     ) +
-#     
-#     theme_minimal(base_family = "Candara") +
-#     
-#     theme(
-#         legend.position = "none", 
-#         
-#         axis.title = element_blank(),
-#         axis.text.y = element_blank(),
-#         axis.text.x = element_text(size = 14, face = "bold", color = "black"),
-#         
-# 
-#         panel.grid.major = element_line(linewidth = 0.35, color = "grey85"),
-#         panel.grid.minor = element_blank(),
-#         
-        # plot.title = element_markdown(size = 18, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
-        # plot.subtitle = element_markdown(size = 12, hjust = 0.5, color = "grey30", margin = margin(t = 2.5, b = 25)),
-        # plot.caption = element_markdown(margin = margin(t = 35), size = 8, hjust = 1, lineheight = 1.2),
-
-#         plot.background = element_rect(fill = "#e4e4e3", color = NA),
-#         plot.margin = margin(20, 20, 20, 20)
-#     )
 
 # save ---------
 
 ggsave(
     plot = gr, filename = "Rplot.png",
-    width = 10, height = 9, units = "in", dpi = 600
+    width = 10.5, height = 9, units = "in", dpi = 600
 )
 
