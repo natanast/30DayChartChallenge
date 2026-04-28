8c2d7bc7c4f7d9985658d18ecc90488715c5d128
NatAnastas <anastasiadounat@gmail.com>
2026-04-28 18:12:32 +0300

    d28

--- 2026/28/28_day.R
+++ 2026/28/28_day.R
@@ -23,31 +23,23 @@ dt <- fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday/maste
 dt$kind <- dt$kind |> factor(levels = c("Human", "AI"), labels = c("Human Text", "AI Text"))
 
 
-bg_light  <- "grey93"
-text_main <- "#1a1a1c"
-
-
-fill_colors <- c("Human Text" = "#94a3b8", "AI Text" = "#466370")
+fill_colors <- c("Human Text" = "#b25c56", "AI Text" = "#466370")
 
 
 # plot ------
 
 gr <- ggplot(dt, aes(x = .pred_AI, y = detector, fill = kind)) +
     
-    # THE VISUALIZATION: Density Ridges
-    # The wider the "hill", the more uncertain the model is.
     geom_density_ridges(
-        alpha = 0.7, 
+        alpha = 0.85, 
         color = "white", 
         linewidth = 0.5,
-        scale = 1.2, # Allows the ribbons to overlap slightly for depth
+        scale = 1.2, 
         rel_min_height = 0.01
     ) +
     
-    # Facet by the actual source of the text
     facet_wrap(~kind) +
     
-    # Format X-axis as percentages
     scale_x_continuous(labels = scales::percent_format(), limits = c(0, 1)) +
     scale_fill_manual(values = fill_colors) +
     
@@ -63,8 +55,7 @@ gr <- ggplot(dt, aes(x = .pred_AI, y = detector, fill = kind)) +
     
     theme(
         legend.position = "none",
-        plot.background = element_rect(fill = bg_light, color = NA),
-        panel.background = element_rect(fill = bg_light, color = NA),
+        plot.background = element_rect(fill = "grey95", color = NA),
         
         strip.text = element_text(face = "bold", size = 12, color = "grey20"),
         strip.background = element_blank(),
@@ -75,7 +66,7 @@ gr <- ggplot(dt, aes(x = .pred_AI, y = detector, fill = kind)) +
         
         
         axis.title.x = element_text(size = 10, color = "grey20", margin = margin(t = 10)),
-        axis.text.y = element_text(color = text_main, size = 11, face = "bold"),
+        axis.text.y = element_text(color = "grey10", size = 11, face = "bold"),
         axis.text.x = element_text(color = "grey40", size = 9),
         
         plot.title = element_markdown(size = 17, face = "bold", hjust = 0.5, margin = margin(t = 15, b = 5)),
